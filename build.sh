08b3c4a63c4199ac99ffa684eca02480484ed47b
Chun-Wei Chen <jacky82226@gmail.com>
2019-05-28 16:41:37 -0700

    pull dockers in build.sh and docker run in notebook

---
+++ build.sh
@@ -0,0 +1,7 @@
+# login to docker
+docker login ziylregistry.azurecr.io
+
+# pull 3 docker images
+docker pull ziylregistry.azurecr.io/onnx-converter
+docker pull ziylregistry.azurecr.io/create-input
+docker pull ziylregistry.azurecr.io/perf_test
