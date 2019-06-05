c1ef94d74edb1d3a0f7ebb81120df6f7eeaa1ee6
Chun-Wei Chen <jacky82226@gmail.com>
2019-06-05 15:04:31 -0700

    remove create_input in build

--- notebook/build.sh
+++ notebook/build.sh
@@ -1,7 +1,4 @@
 # login to docker
 docker login ziylregistry.azurecr.io
-
 # pull 3 docker images
-docker pull ziylregistry.azurecr.io/onnx-converter
-docker pull ziylregistry.azurecr.io/create-input
-docker pull ziylregistry.azurecr.io/perf_test
+docker pull ziylregistry.azurecr.io/onnx-converter && docker pull ziylregistry.azurecr.io/perf_test
