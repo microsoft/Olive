7e8acfde18550dc4602746cec954e4aaf1980278
Cecilia Liu <ziyl@microsoft.com>
2019-08-29 15:39:17 -0700

    update image to mcr for build.sh

--- notebook/build.sh
+++ notebook/build.sh
@@ -1,4 +1,4 @@
-# login to docker
-docker login ziylregistry.azurecr.io -u ziylregistry
+# # login to docker
+# docker login ziylregistry.azurecr.io -u ziylregistry
 # pull 2 docker images
-docker pull ziylregistry.azurecr.io/onnx-converter && docker pull ziylregistry.azurecr.io/perf-test
+docker pull mcr.microsoft.com/onnxruntime/onnx-converter && docker pull mcr.microsoft.com/onnxruntime/perf-tuning
