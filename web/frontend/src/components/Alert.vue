ed18898b9ad2f4ceaf00dc840127797201a1500f
Cecilia Liu <ziyl@microsoft.com>
2019-08-29 19:08:20 -0700

    loading

--- web/frontend/src/components/Alert.vue
+++ web/frontend/src/components/Alert.vue
@@ -2,7 +2,7 @@
   <div style="white-space: pre-line;">
     <b-alert variant="success" show>{{ message }}</b-alert>
     <b-progress 
-      :v-if=loading
+      v-if="loading"
       :value="100" variant="success" striped class="mt-2"></b-progress>
     <br>
   </div>
