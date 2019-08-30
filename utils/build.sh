930c3bd9bbcaf73c7ae4450d5b1ce3432a8e9bfd
Cecilia Liu <ziyl@microsoft.com>
2019-08-30 14:26:53 -0700

    onnxpipeline

---
+++ utils/build.sh
@@ -0,0 +1,5 @@
+# login to docker
+# docker login ziylregistry.azurecr.io -u ziylregistry
+# pull 2 docker images
+docker pull mcr.microsoft.com/onnxruntime/onnx-converter && docker pull mcr.microsoft.com/onnxruntime/perf-tuning
+
