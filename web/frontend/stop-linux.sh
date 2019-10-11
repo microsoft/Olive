a9fef8582fc4dc800f82b09ea518f9a1c5d6d272
CeciliaLiu <ziyue.liu7@gmail.com>
2019-10-11 10:22:37 -0700

    stop linux script

---
+++ web/frontend/stop-linux.sh
@@ -0,0 +1,4 @@
+pkill -9 -f 'celery worker'
+kill -9 `sudo lsof -t -i:8000`
+kill -9 `sudo lsof -t -i:5555`
+kill -9 `sudo lsof -t -i:6379`
--- web/start-linux.sh
+++ web/start-linux.sh
@@ -8,7 +8,3 @@ celery flower -A app.celery --port=5555 &>/dev/null &
 celery worker -A app.celery -P gevent &>/dev/null &
 cd ..
 python ./backend/app.py
-pkill -9 -f 'celery worker'
-kill -9 `sudo lsof -t -i:8000`
-kill -9 `sudo lsof -t -i:5555`
-kill -9 `sudo lsof -t -i:6379`
