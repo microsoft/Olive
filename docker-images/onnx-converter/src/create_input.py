a8582f7d5340fc5d4391836d313d522a9deca2e1
Cecilia Liu <ziyl@microsoft.com>
2020-04-09 15:00:38 -0700

    Add test_data_path to onnx-converter

--- docker-images/onnx-converter/src/create_input.py
+++ docker-images/onnx-converter/src/create_input.py
@@ -34,31 +34,61 @@ TYPE_MAP = {
     'tensor(double)': np.float64, 
 }
 
-def generate_inputs(model):
-    
-    # Create a test folder path
-    model_dir = os.path.dirname(os.path.abspath(model))
-
-    # Check if test folder and test data already exists
+def search_for_existing_test_data(user_data_path, output_data_path):
+    if not os.path.exists(user_data_path):
+        print('No data found under user provided folder. Generating random input data.')
+        return
     regex = re.compile("test_data*")
-    for f in os.listdir(model_dir):
+    test_folder_name = 'test_data_set_'
+    test_folder_idx = 0
+    for f in os.listdir(user_data_path):
         if regex.match(f):
-            test_path = os.path.join(model_dir, f)
-            for inputFiles in os.listdir(test_path):
-                if inputFiles.endswith('.pb') and inputFiles.startswith("input"):
-                    print("Input.pb already exists. Skipping dummy input generation. ")
-                    return test_path    
-                    
-    test_path = os.path.join(model_dir, "test_data_set_0")
+            cur_user_data_path = os.path.join(user_data_path, f)
+            for inputFiles in os.listdir(cur_user_data_path):
+                if inputFiles.endswith('.pb') == False:
+                    print('No data found under user provided folder. Generating random input data.')
+                    return                  
+            print('Test data .pb files found under {}. '.format(cur_user_data_path))
+            # Copy the test data to the ONNX output dir. Overwrite if already exists.
+            cur_test_folder_path = os.path.join(output_data_path, test_folder_name + str(test_folder_idx))
+            print('copying {} to {}'.format(cur_user_data_path, os.path.join(output_data_path, test_folder_name + str(test_folder_idx))))
+            if os.path.exists(cur_test_folder_path):
+                shutil.rmtree(cur_test_folder_path)
+            shutil.copytree(cur_user_data_path, cur_test_folder_path)
+            test_folder_idx += 1
+
+def generate_inputs(input_model_path, src_test_data, output_model_path):
+    # Create a test folder path
+    output_test_data_dir = os.path.dirname(os.path.abspath(output_model_path))
+    test_path = os.path.join(output_test_data_dir, 'test_data_set_0')
+    
+    regex = re.compile("test_data*")
+    # Find and copy over the user specified test data
+    if src_test_data:
+        search_for_existing_test_data(src_test_data, output_test_data_dir)
+    else:
+        potential_data_dir = os.path.dirname(os.path.abspath(input_model_path))
+        search_for_existing_test_data(potential_data_dir, output_test_data_dir)
+    
     if not os.path.exists(test_path):
         os.mkdir(test_path)
         os.chmod(test_path, 0o644)
-    # Get the input model
-    sess = rt.InferenceSession(model)
+    # Check if test folder and test data already exists
+    regex = re.compile("test_data*")
+    for f in os.listdir(output_test_data_dir):
+        if regex.match(f):
+            user_data_path = os.path.join(output_test_data_dir, f)
+            for inputFiles in os.listdir(user_data_path):
+                if inputFiles.endswith('.pb'):
+                    print("Test data .pb files already exist. Skipping dummy input generation. ")
+                    return test_path
+
+    # Get input names from converted model
+    sess = rt.InferenceSession(output_model_path)
 
     #########################
     # Let's see the input name and shape.
-    print("%s inputs: " % model)
+    print("%s inputs: " % output_model_path)
     inputs = sess.get_inputs()
     for i in range(0, len(inputs)):
         print("input name: %s, shape: %s, type: %s" % (inputs[i].name, inputs[i].shape, inputs[i].type))
