082f6de46b44b0f0be7d4e2c1e715c3a997cbb45
Anurag Kumar <mailanu98@gmail.com>
2022-01-14 21:23:35 +0530

    updated classifiers
    added support for python 3.8, 3.9, 3.10

--- setup.py
+++ setup.py
@@ -23,6 +23,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.10"
         ],
     packages=find_packages(),
     package_data=get_package_data(),
