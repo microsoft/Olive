4e000204819a50c756f2d449a708157e7550e35c
Justin Stoecker <justoeck@microsoft.com>
2023-03-31 19:01:28 -0700

    use pass config values

--- olive/passes/onnx/float16_conversion.py
+++ olive/passes/onnx/float16_conversion.py
@@ -26,7 +26,7 @@ class OnnxFloatToFloat16(Pass):
                 type_=float, default=1e-7, description=("Constant values will be clipped against this value")
             ),
             "max_finite_val": PassConfigParam(
-                type_=float, default=1e-7, description=("Constant values will be clipped against this value")
+                type_=float, default=1e4, description=("Constant values will be clipped against this value")
             ),
             "keep_io_types": PassConfigParam(
                 type_=bool, default=False, description=("Whether model inputs/outputs should be left as float32")
@@ -45,9 +45,19 @@ class OnnxFloatToFloat16(Pass):
     def _run_for_config(self, model: ONNXModel, config: Dict[str, Any], output_model_path: str) -> ONNXModel:
         if Path(output_model_path).suffix != ".onnx":
             output_model_path += ".onnx"
+        
+        config = self._config_class(**config)
 
         model_fp32 = onnx.load(str(model.model_path))
-        model_fp16 = float16.convert_float_to_float16(model_fp32)
+        model_fp16 = float16.convert_float_to_float16(
+            model_fp32,
+            min_positive_val=config.min_positive_val,
+            max_finite_val=config.max_finite_val,
+            keep_io_types=config.keep_io_types,
+            disable_shape_infer=config.disable_shape_infer,
+            op_block_list=config.op_block_list,
+            node_block_list=config.node_block_list
+        )
         onnx.save(model_fp16, output_model_path)
 
         return ONNXModel(output_model_path, model.name)
