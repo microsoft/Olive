643620f029b537594f2670f483cb582a4c0381b7
Xiaoyu <xiaoyuzhang@microsoft.com>
2023-04-18 16:05:42 +0000

    Add openvino pass unit tests

---
+++ test/unit_test/passes/openvino/test_openvino_conversion.py
@@ -0,0 +1,51 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+import tempfile
+from pathlib import Path
+from test.unit_test.utils import get_pytorch_model, get_pytorch_model_dummy_input
+
+from olive.passes.openvino.conversion import OpenVINOConversion
+from olive.systems.local import LocalSystem
+
+
+def test_openvino_conversion_pass():
+    # setup
+    local_system = LocalSystem()
+    input_model = get_pytorch_model()
+    dummy_input = get_pytorch_model_dummy_input()
+    openvino_conversion_config = {"extra_config": {"example_input": dummy_input}}
+
+    p = OpenVINOConversion(openvino_conversion_config, disable_search=True)
+    with tempfile.TemporaryDirectory() as tempdir:
+        output_folder = str(Path(tempdir) / "openvino")
+
+        # execute
+        openvino_model = local_system.run_pass(p, input_model, output_folder)
+
+        # assert
+        assert Path(openvino_model.model_path).exists()
+        assert (Path(openvino_model.model_path) / "ov_model.bin").is_file()
+        assert (Path(openvino_model.model_path) / "ov_model.xml").is_file()
+
+
+def test_openvino_conversion_pass_no_example_input():
+    # setup
+    local_system = LocalSystem()
+    input_model = get_pytorch_model()
+    openvino_conversion_config = {
+        "input_shape": [1, 10],
+    }
+
+    p = OpenVINOConversion(openvino_conversion_config, disable_search=True)
+    with tempfile.TemporaryDirectory() as tempdir:
+        output_folder = str(Path(tempdir) / "openvino")
+
+        # execute
+        openvino_model = local_system.run_pass(p, input_model, output_folder)
+
+        # assert
+        assert Path(openvino_model.model_path).exists()
+        assert (Path(openvino_model.model_path) / "ov_model.bin").is_file()
+        assert (Path(openvino_model.model_path) / "ov_model.xml").is_file()
