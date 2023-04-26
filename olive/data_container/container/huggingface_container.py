2a1919670a3b10403b801cd4f709db9a10541725
trajepl <jiapli@microsoft.com>
2023-04-26 15:07:46 +0800

    linter fix

--- olive/data_container/container/huggingface_container.py
+++ olive/data_container/container/huggingface_container.py
@@ -11,6 +11,7 @@ from olive.data_container.registry import Registry
 @Registry.register(DataContainerType.DATA_CONTAINER)
 class HuggingfaceContainer(BaseContainer):
     from transformers.pipelines import SUPPORTED_TASKS
+
     _dataset: str = "huggingface_dataset"
     _pre_process: str = "huggingface_pre_process"
 
