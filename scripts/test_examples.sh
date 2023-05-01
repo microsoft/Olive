0341ea6d975fb17d5a2d1461ffbc55198da4384b
Jambay Kinley <jambaykinley@microsoft.com>
2023-05-01 22:06:13 +0000

    update whisper reqs

--- scripts/test_examples.sh
+++ scripts/test_examples.sh
@@ -8,7 +8,6 @@ set -eoux pipefail
 PIPELINE=$1
 ROOT_DIR=$2
 EXAMPLE=$3
-INSTALL_INDEX=$4
 
 echo $PIPELINE
 if [[ "$PIPELINE" == "True" ]]; then
@@ -27,11 +26,10 @@ python -m pip install -r $ROOT_DIR/examples/$EXAMPLE/requirements.txt
 # TODO: need to remove later
 echo "Installing custom packages for whisper"
 if [[ "$EXAMPLE" == "whisper" ]]; then
-    python -m pip uninstall -y onnxruntime-extensions
+    python -m pip uninstall -y onnxruntime onnxruntime-extensions
+    python -m pip install ort-nightly==1.15.0.dev20230429003 --index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple/
     export OCOS_NO_OPENCV=1
     python -m pip install git+https://github.com/microsoft/onnxruntime-extensions.git
-    python -m pip uninstall -y onnxruntime
-    python -m pip install --index-url $INSTALL_INDEX onnxruntime
 fi
 
 python -m pytest -v -s --log-cli-level=WARNING --junitxml=$ROOT_DIR/logs/test_examples-TestOlive.xml $ROOT_DIR/examples/test_$EXAMPLE.py
