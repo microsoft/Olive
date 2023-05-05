713e09c3f24d1d40ae3c4f442978ac31a1a5ca93
Jambay Kinley <jambaykinley@microsoft.com>
2023-05-04 21:18:52 -0700

    Correct logger severity mapping (#238)
    ## Describe your changes
    `olive.set_default_logger_severity` takes int from 0-4 for severity
    level. The corresponding levels for `logging` are 10-50, not 0-40.
    Update the mapping for this.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    
    ## (Optional) Issue link

--- olive/__init__.py
+++ olive/__init__.py
@@ -20,6 +20,15 @@ __version__ = "0.1.0"
 def set_default_logger_severity(level):
     """
     Set log level for olive package.
+
     :param level: 0: DEBUG, 1: INFO, 2: WARNING, 3: ERROR, 4: CRITICAL
     """
-    _logger.setLevel(level * 10)
+    # mapping from level to logging level
+    level_map = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING, 3: logging.ERROR, 4: logging.CRITICAL}
+
+    # check if level is valid
+    if level not in level_map:
+        raise ValueError(f"Invalid level {level}, should be one of {list(level_map.keys())}")
+
+    # set logger level
+    _logger.setLevel(level_map[level])
