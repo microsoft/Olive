3afa5ad2a0ba954e3799a4a136b2fed011312ca6
leqiao <leqiao@microsoft.com>
2023-05-17 13:56:17 +0800

    fix test

--- examples/bert_ptq_cpu/preprare_mlflow_model.py
+++ examples/bert_ptq_cpu/preprare_mlflow_model.py
@@ -2,6 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+import os
+import shutil
+
 import transformers
 from azureml.evaluate import mlflow as aml_mlflow
 
@@ -15,6 +18,10 @@ def prepare_model():
     hf_conf = {
         "task_type": task,
     }
+
+    if os.path.exists(model_path):
+        shutil.rmtree(model_path)
+
     aml_mlflow.hftransformers.save_model(
         original_model,
         model_path,
