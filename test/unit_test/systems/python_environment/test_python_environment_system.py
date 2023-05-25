584158dea71a6935ea50a1cf552dfa45c4c57eef
Jambay Kinley <jambaykinley@microsoft.com>
2023-05-25 23:49:29 +0000

    save for now

--- test/unit_test/systems/python_environment/test_python_environment_system.py
+++ test/unit_test/systems/python_environment/test_python_environment_system.py
@@ -2,17 +2,24 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+import pickle
 import sys
 from pathlib import Path
 from test.unit_test.utils import get_accuracy_metric, get_latency_metric, get_onnx_model
-from unittest.mock import patch
+from unittest import mock
+from unittest.mock import MagicMock, patch
 
+import numpy as np
 import pytest
 
 from olive.evaluator.metric import AccuracySubType, LatencySubType, MetricResult, MetricType, joint_metric_key
-from olive.evaluator.olive_evaluator import OliveEvaluatorFactory
+
+# from olive.evaluator.olive_evaluator import OliveEvaluatorFactory
 from olive.hardware import DEFAULT_CPU_ACCELERATOR
 from olive.systems.python_environment import PythonEnvironmentSystem
+from olive.systems.python_environment.available_eps import main as available_eps_main
+from olive.systems.python_environment.inference_runner import main as inference_runner_main
+from olive.systems.python_environment.is_valid_ep import main as is_valid_ep_main
 
 
 class TestPythonEnvironmentSystem:
@@ -22,7 +29,7 @@ class TestPythonEnvironmentSystem:
         executable_parent = Path(sys.executable).parent.resolve().as_posix()
         self.system = PythonEnvironmentSystem(executable_parent)
 
-    def test_available_eps(self):
+    def test_get_supported_execution_providers(self):
         import onnxruntime as ort
 
         assert set(self.system.get_supported_execution_providers()) == set(ort.get_available_providers())
@@ -64,10 +71,8 @@ class TestPythonEnvironmentSystem:
         assert mock_evaluate_accuracy.call_once_with(model, metrics[0])
         assert mock_evaluate_latency.call_once_with(model, metrics[1])
 
-    @pytest.mark.skip(reason="Unable to patch static function calls from another function")
     @patch("olive.evaluator.olive_evaluator.OliveEvaluator.compute_accuracy")
-    @patch("olive.systems.python_environment.python_environment_system.OliveEvaluator.compute_accuracy")
-    def test_evaluate_accuracy(self, mock_compute_accuracy1, mock_compute_accuracy2):
+    def test_evaluate_accuracy(self, mock_compute_accuracy):
         # setup
         model = get_onnx_model()
         metric = get_accuracy_metric(AccuracySubType.ACCURACY_SCORE, random_dataloader=False)
@@ -81,19 +86,19 @@ class TestPythonEnvironmentSystem:
             }
         )
 
-        mock_compute_accuracy1.return_value = mock_value
-        mock_compute_accuracy2.return_value = mock_value
+        mock_compute_accuracy.return_value = mock_value
 
         # expected result
-        evaluator = OliveEvaluatorFactory.create_evaluator_for_model(model)
-        expected_res = evaluator.evaluate(model, [metric])[metric.name]
+        expected_res = 0.9
 
         # execute
         actual_res = self.system.evaluate_accuracy(model, metric)
 
         # assert
-        assert actual_res[AccuracySubType.ACCURACY_SCORE].value == expected_res[AccuracySubType.ACCURACY_SCORE].value
-        assert mock_compute_accuracy1.call_args.args[1] == mock_compute_accuracy2.call_args.args[1]
+        assert actual_res[AccuracySubType.ACCURACY_SCORE].value == expected_res
+        # assert mock_compute_accuracy.call_once_with(model, metric)
+        print(mock_compute_accuracy.call_args.args[1])
+        # assert mock_compute_accuracy1.call_args.args[1] == mock_compute_accuracy2.call_args.args[1]
 
     @patch("olive.evaluator.olive_evaluator.OliveEvaluator.compute_latency")
     def test_evaluate_latency(self, mock_compute_latency):
@@ -124,3 +129,168 @@ class TestPythonEnvironmentSystem:
         assert actual_res[LatencySubType.AVG].value == expected_res
         assert len(mock_compute_latency.call_args.args[1]) == metric_config.repeat_test_num
         assert all([latency > 0 for latency in mock_compute_latency.call_args.args[1]])
+
+    @patch("onnxruntime.get_available_providers")
+    def test_available_eps_script(self, mock_get_providers, tmp_path):
+        mock_get_providers.return_value = ["CPUExecutionProvider"]
+        output_path = tmp_path / "available_eps.pkl"
+
+        # command
+        args = ["--output_path", str(output_path)]
+
+        # execute
+        available_eps_main(args)
+
+        # assert
+        assert output_path.exists()
+        mock_get_providers.assert_called_once()
+        with open(output_path, "rb") as f:
+            assert pickle.load(f) == ["CPUExecutionProvider"]
+
+    @patch("olive.systems.python_environment.is_valid_ep.get_ort_inference_session")
+    def test_is_valid_ep_script_success(self, mock_get_session, tmp_path):
+        mock_get_session.return_value = None
+        output_path = tmp_path / "is_valid_ep.pkl"
+
+        # command
+        args = ["--model_path", "model.onnx", "--ep", "CPUExecutionProvider", "--output_path", str(output_path)]
+
+        # execute
+        is_valid_ep_main(args)
+
+        # assert
+        assert output_path.exists()
+        mock_get_session.assert_called_once_with("model.onnx", {"execution_provider": "CPUExecutionProvider"})
+        with open(output_path, "rb") as f:
+            assert pickle.load(f) == {"valid": True}
+
+    @patch("olive.systems.python_environment.is_valid_ep.get_ort_inference_session")
+    def test_is_valid_ep_script_fail(self, mock_get_session, tmp_path):
+        mock_get_session.side_effect = Exception("Mock Failure")
+        output_path = tmp_path / "is_valid_ep.pkl"
+
+        # command
+        args = ["--model_path", "model.onnx", "--ep", "CPUExecutionProvider", "--output_path", str(output_path)]
+
+        # execute
+        is_valid_ep_main(args)
+
+        # assert
+        assert output_path.exists()
+        mock_get_session.assert_called_once_with("model.onnx", {"execution_provider": "CPUExecutionProvider"})
+        with open(output_path, "rb") as f:
+            assert pickle.load(f) == {"valid": False, "error": "Mock Failure"}
+
+    @patch("olive.systems.python_environment.inference_runner.get_ort_inference_session")
+    def test_inference_runner_script_accuracy(self, mock_get_session, tmp_path):
+        # setup
+        mock_inference_session = MagicMock()
+        dummy_output = np.array([1, 2, 3])
+        mock_inference_session.run.return_value = dummy_output
+        mock_get_session.return_value = mock_inference_session
+
+        model = "model.onnx"
+        inference_settings = {"execution_provider": "CPUExecutionProvider"}
+        inference_settings_path = tmp_path / "inference_settings.pkl"
+        with open(inference_settings_path, "wb") as f:
+            pickle.dump(inference_settings, f)
+        input_dir = tmp_path / "input"
+        input_dir.mkdir(parents=True, exist_ok=True)
+        num_batches = 2
+        for i in range(num_batches):
+            np.savez(input_dir / f"input_{i}.npz", **{"input": np.array([i])})
+        output_dir = tmp_path / "output"
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+        # command
+        args = [
+            "--type",
+            "accuracy",
+            "--model_path",
+            model,
+            "--input_dir",
+            str(input_dir),
+            "--output_dir",
+            str(output_dir),
+            "--inference_settings_path",
+            str(inference_settings_path),
+            "--num_batches",
+            str(num_batches),
+        ]
+
+        # execute
+        inference_runner_main(args)
+
+        # assert
+        assert mock_get_session.call_once_with(model, inference_settings)
+        assert mock_inference_session.run.call_count == num_batches
+        assert mock_inference_session.run.mock_calls == [
+            mock.call(input_feed={"input": np.array([i])}, output_names=None) for i in range(num_batches)
+        ]
+        for i in range(num_batches):
+            assert (output_dir / f"output_{i}.npy").exists()
+            np.load(output_dir / f"output_{i}.npy") == dummy_output
+
+    @pytest.mark.parametrize("io_bind", [True, False])
+    @patch("olive.systems.python_environment.inference_runner.get_ort_inference_session")
+    def test_inference_runner_script_latency(self, mock_get_session, tmp_path, io_bind):
+        # setup
+        # inference session
+        mock_inference_session = MagicMock()
+        mock_inference_session.run.return_value = None
+        mock_inference_session.run_with_iobinding.return_value = None
+        # io binding
+        io_bind_op = MagicMock()
+        mock_inference_session.io_binding.return_value = io_bind_op
+        # get session
+        mock_get_session.return_value = mock_inference_session
+
+        model = "model.onnx"
+        inference_settings = {"execution_provider": "CPUExecutionProvider"}
+        inference_settings_path = tmp_path / "inference_settings.pkl"
+        with open(inference_settings_path, "wb") as f:
+            pickle.dump(inference_settings, f)
+        input_dir = tmp_path / "input"
+        input_dir.mkdir(parents=True, exist_ok=True)
+        np.savez(input_dir / "input.npz", **{"input": np.array([1])})
+        output_dir = tmp_path / "output"
+        output_dir.mkdir(parents=True, exist_ok=True)
+        warmup_num = 1
+        repeat_test_num = 3
+
+        # command
+        args = [
+            "--type",
+            "latency",
+            "--model_path",
+            model,
+            "--input_dir",
+            str(input_dir),
+            "--output_dir",
+            str(output_dir),
+            "--inference_settings_path",
+            str(inference_settings_path),
+            "--warmup_num",
+            str(warmup_num),
+            "--repeat_test_num",
+            str(repeat_test_num),
+        ]
+        if io_bind:
+            args += ["--io_bind", "--device", "cpu"]
+
+        # execute
+        inference_runner_main(args)
+
+        # assert
+        total_num = warmup_num + repeat_test_num
+        assert mock_get_session.call_once_with(model, inference_settings)
+        if not io_bind:
+            assert mock_inference_session.run.call_count == total_num
+            assert mock_inference_session.run.mock_calls == [
+                mock.call(input_feed={"input": np.array([1])}, output_names=None) for _ in range(total_num)
+            ]
+        else:
+            assert mock_inference_session.run_with_iobinding.call_count == total_num
+            assert mock_inference_session.run_with_iobinding.mock_calls == [
+                mock.call(io_bind_op) for _ in range(total_num)
+            ]
