9ad655c007a1b0e775b8dde469da8fd141b3fc86
leqiao <leqiao@microsoft.com>
2023-05-29 15:11:55 +0800

    modify test

--- test/unit_test/workflows/test_setup.py
+++ test/unit_test/workflows/test_setup.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-import pytest
-
 from olive.workflows.run.config import RunConfig
 from olive.workflows.run.run import dependency_setup
 
@@ -9,7 +7,4 @@ from olive.workflows.run.run import dependency_setup
 def test_dependency_setup():
     user_script_config_file = Path(__file__).parent / "mock_data" / "user_script.json"
     run_config = RunConfig.parse_file(user_script_config_file)
-    try:
-        dependency_setup(run_config)
-    except Exception as e:
-        pytest.fail(f"setup failed with an exception: {str(e)}")
+    dependency_setup(run_config)
