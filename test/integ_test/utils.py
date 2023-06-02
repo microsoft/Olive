f780d7c8b0d9530a6bf9803dd4f2872f60d39269
trajep <jiapli@microsoft.com>
2023-06-02 13:50:45 +0800

    🐝 Remove ort version dependency and refine integration test utils (#315)
    ## Describe your changes
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

---
+++ test/integ_test/utils.py
@@ -0,0 +1,42 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+import os
+
+from azure.storage.blob import BlobClient
+
+
+def get_olive_workspace_config():
+    subscription_id = os.environ.get("WORKSPACE_SUBSCRIPTION_ID")
+    if subscription_id is None:
+        raise Exception("Please set the environment variable WORKSPACE_SUBSCRIPTION_ID")
+
+    resource_group = os.environ.get("WORKSPACE_RESOURCE_GROUP")
+    if resource_group is None:
+        raise Exception("Please set the environment variable WORKSPACE_RESOURCE_GROUP")
+
+    workspace_name = os.environ.get("WORKSPACE_NAME")
+    if workspace_name is None:
+        raise Exception("Please set the environment variable WORKSPACE_NAME")
+
+    workspace_config = {
+        "subscription_id": subscription_id,
+        "resource_group": resource_group,
+        "workspace_name": workspace_name,
+    }
+
+    return workspace_config
+
+
+def download_azure_blob(container, blob, download_path):
+    try:
+        conn_str = os.environ["OLIVEWHEELS_STORAGE_CONNECTION_STRING"]
+    except KeyError:
+        raise Exception("Please set the environment variable OLIVEWHEELS_STORAGE_CONNECTION_STRING")
+
+    blob = BlobClient.from_connection_string(conn_str=conn_str, container_name=container, blob_name=blob)
+
+    with open(download_path, "wb") as my_blob:
+        blob_data = blob.download_blob()
+        blob_data.readinto(my_blob)
