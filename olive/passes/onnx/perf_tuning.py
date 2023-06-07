b8eadba39b4e28989278a221d59f239573f34cb7
Jambay Kinley <jambaykinley@microsoft.com>
2023-06-06 21:50:46 -0700

    Use `data_config` for ort perf tuning (#324)
    ## Describe your changes
    Use the provided `data_config` for perf tuning. We only infer model io
    config to create dummy data if the user doesn't provide
    input_names/input_shapes or data config.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- olive/passes/onnx/perf_tuning.py
+++ olive/passes/onnx/perf_tuning.py
@@ -58,9 +58,26 @@ def tune_onnx_model(model, config):
     config_dict = config.dict()
 
     # use model io_config if user does not specify input_names and input_shapes
-    if not config_dict.get("input_names") or not config_dict.get("input_shapes"):
+    # only do this if data_config or dataloader is not provided
+    # priority: dataloader_func > input_names/input_shapes > data_config > model io_config
+    if not (
+        config_dict.get("dataloader_func")
+        or config_dict.get("data_config")
+        or (config_dict.get("input_names") and config_dict.get("input_shapes"))
+    ):
+        logger.debug(
+            "dataloader_func, data_config and input_names/input_shapes are not provided. Use model io_config instead."
+        )
         io_config = model.get_io_config()
         config_dict["input_names"] = io_config["input_names"]
+        # check if inferred input shapes are static
+        input_shapes = io_config["input_shapes"]
+        is_static = all(all(isinstance(dim, int) for dim in shape) for shape in input_shapes)
+        if not is_static:
+            logger.debug(
+                "Model input shapes are not static. Cannot use infered input shapes for creating dummy data. This will"
+                " cause an error when creating dummy data for tuning."
+            )
         config_dict["input_shapes"] = io_config["input_shapes"]
         config_dict["input_types"] = io_config["input_types"]
 
@@ -68,9 +85,16 @@ def tune_onnx_model(model, config):
         if eval_config in config_dict:
             latency_user_config[eval_config] = config_dict.get(eval_config)
     latency_sub_types = [{"name": LatencySubType.AVG}]
-    latency_metric = Metric(
-        name="latency", type=MetricType.LATENCY, sub_types=latency_sub_types, user_config=latency_user_config
-    )
+    latency_metric_config = {
+        "name": "latency",
+        "type": MetricType.LATENCY,
+        "sub_types": latency_sub_types,
+        "user_config": latency_user_config,
+    }
+    if config_dict.get("data_config"):
+        # we have to do this condition since data_config cannot be None
+        latency_metric_config["data_config"] = config_dict.get("data_config")
+    latency_metric = Metric(**latency_metric_config)
 
     pretuning_inference_result = get_benchmark(model, latency_metric, config)
 
