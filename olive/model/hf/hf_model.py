4508c12ad6f4698ebf3fac333c9ae349341e9eae
Xiaoyu <xiaoyuzhang@microsoft.com>
2023-06-07 22:56:31 +0000

    Update doc

--- olive/model/hf/hf_model.py
+++ olive/model/hf/hf_model.py
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
-from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Union
 
 import torch
@@ -14,9 +13,9 @@ from olive.model.model_config import IOConfig
 
 class HFComponent(ConfigBase):
     name: str
-    io_config: Optional[Union[IOConfig, str, Dict[str, Any]]] = None
-    component_func: Optional[Union[str, Callable]] = None
-    dummy_inputs_func: Optional[Union[str, Callable]] = None
+    io_config: Union[IOConfig, str, Dict[str, Any]]
+    component_func: Union[str, Callable]
+    dummy_inputs_func: Union[str, Callable]
 
 
 class HFConfig(ConfigBase):
@@ -25,8 +24,6 @@ class HFConfig(ConfigBase):
     feature: str = "default"
     # TODO: remove model_class and only use task
     model_class: str = None
-    model_type: str = None
-    state_dict_path: Union[str, Path] = None
     use_custom_implementation: bool = False
     components: List[HFComponent] = None
     config: Dict[str, Any] = None
@@ -38,18 +35,6 @@ class HFConfig(ConfigBase):
             if not v and not values.get("task", None):
                 raise ValueError("Either task or model_class must be specified")
             return v
-
-    @validator("components")
-    def validate_components(cls, v, values):
-        if values["use_custom_implementation"]:
-            for component in v:
-                if component.io_config is None:
-                    raise ValueError("io_config must be specified for all components")
-                if component.component_func is None:
-                    raise ValueError("component_func must be specified for all components")
-                if component.dummy_inputs_func is None:
-                    raise ValueError("dummy_inputs_func must be specified for all components")
-        return v
 
 
 class HFModelBase(torch.nn.Module):
