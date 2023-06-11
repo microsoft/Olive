5ec0a52c973f1addd2a0491e2fdf38d5e2b56224
Xiaoyu <85524621+xiaoyu-work@users.noreply.github.com>
2023-06-10 19:36:08 -0700

    Add missing copyright header (#339)
    ## Describe your changes
    Add missing copyright header
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- olive/model/model_config.py
+++ olive/model/model_config.py
@@ -1,3 +1,7 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
 from typing import Dict, List
 
 from pydantic import validator
