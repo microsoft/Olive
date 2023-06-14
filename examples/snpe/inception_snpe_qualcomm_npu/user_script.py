ef2b0518169b12f9fc5611f4ed9e60481cafc255
Jambay Kinley <jambaykinley@microsoft.com>
2023-06-13 20:25:50 -0700

    Register custom data config components using user script (#348)
    ## Describe your changes
    Add `user_script` and `script_dir` to `DataConfig` and load the user
    script during initialization so that the custom components are
    registered.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- examples/snpe/inception_snpe_qualcomm_npu/user_script.py
+++ examples/snpe/inception_snpe_qualcomm_npu/user_script.py
@@ -2,8 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+from olive.data.registry import Registry
 
 
-# TODO: Support registering custom components for data config
-def post_process(output):
+@Registry.register_post_process()
+def inception_post_process(output):
     return output["results"]["InceptionV3/Predictions/Reshape_1:0"].squeeze(1).argmax(axis=1)
