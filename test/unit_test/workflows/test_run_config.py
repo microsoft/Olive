42cd9f44003e3d13702173df8b4826220e9d73ad
trajep <jiapli@microsoft.com>
2023-06-20 10:38:36 +0800

    📤Expose params of DefaultAzureCredential (#362)
    ## Describe your changes
    Expose params of DefaultAzureCredential. Then customer can disable
    unrequired credential getting ways.
    
    ## Checklist before requesting a review
    - [x] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [x] Update documents if necessary.
    - [x] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- test/unit_test/workflows/test_run_config.py
+++ test/unit_test/workflows/test_run_config.py
@@ -5,6 +5,7 @@
 
 import json
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
@@ -52,6 +53,51 @@ class TestRunConfig:
         with pytest.raises(ValueError) as e:
             RunConfig.parse_obj(user_script_config)
             assert str(e.value) == "AzureML client config is required for AzureML system"
+
+    @patch("azure.identity._credentials.default.InteractiveBrowserCredential")
+    @patch("azure.identity._credentials.default.AzurePowerShellCredential")
+    @patch("azure.identity._credentials.default.AzureCliCredential")
+    @patch("azure.identity._credentials.default.SharedTokenCacheCredential")
+    @patch("azure.identity._credentials.default.ManagedIdentityCredential")
+    @patch("azure.identity._credentials.default.EnvironmentCredential")
+    def test_config_with_azureml_default_auth_params(
+        self,
+        mocked_env_credential,
+        mocked_managed_identity_credential,
+        mocked_shared_token_cache_credential,
+        mocked_azure_cli_credential,
+        mocked_azure_powershell_credential,
+        mocked_interactive_browser_credential,
+    ):
+        # we need to mock all the credentials because the default credential will get tokens from all of them
+        with open(self.user_script_config_file, "r") as f:
+            user_script_config = json.load(f)
+
+        user_script_config["azureml_client"]["default_auth_params"] = None
+        config = RunConfig.parse_obj(user_script_config)
+        config.azureml_client.create_client()
+        mocked_env_credential.assert_called_once()
+        mocked_interactive_browser_credential.assert_not_called()
+
+        user_script_config["azureml_client"]["default_auth_params"] = {
+            "exclude_environment_credential": True,
+            "exclude_managed_identity_credential": True,
+        }
+        config = RunConfig.parse_obj(user_script_config)
+        config.azureml_client.create_client()
+        assert mocked_env_credential.call_count == 1
+        assert mocked_managed_identity_credential.call_count == 1
+        assert mocked_azure_cli_credential.call_count == 2
+
+        user_script_config["azureml_client"]["default_auth_params"] = {
+            "exclude_environment_credential": True,
+            "exclude_managed_identity_credential": False,
+        }
+        config = RunConfig.parse_obj(user_script_config)
+        config.azureml_client.create_client()
+        assert mocked_env_credential.call_count == 1
+        assert mocked_managed_identity_credential.call_count == 2
+        assert mocked_azure_cli_credential.call_count == 3
 
     def test_readymade_system(self):
         readymade_config_file = Path(__file__).parent / "mock_data" / "readymade_system.json"
