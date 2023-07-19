a85336bd45ef34685bb08ea12a2c2d1ef2f8fe40
Patrice Vignola <vignola.patrice@gmail.com>
2023-07-18 17:06:05 -0700

    Remove temporary test file

--- examples/directml/stable_diffusion/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from diffusers import DiffusionPipeline
-
-image = np.zeros([1, 1024, 1024, 3])
-pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-0.9")
-prompt = "An astronaut riding a green horse"
-images = pipe(prompt=prompt, image=image).images
