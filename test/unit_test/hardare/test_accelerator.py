95e9f0703d5f2d0df6da16b7743aa96d3e0c65f3
jiapli <jiapli@microsoft.com>
2023-07-26 15:11:50 +0800

    Accelerator assert fix

--- test/unit_test/hardare/test_accelerator.py
+++ test/unit_test/hardare/test_accelerator.py
@@ -14,10 +14,12 @@ class TestAcceleratorLookup:
             (["CPUExecutionProvider"], None),
             (["CUDAExecutionProvider"], ["gpu"]),
             (["DmlExecutionProvider", "CUDAExecutionProvider"], ["gpu"]),
-            (["QNNExecutionProvider", "CUDAExecutionProvider"], ["npu", "gpu"]),
+            (["QNNExecutionProvider", "CUDAExecutionProvider"], ["gpu", "npu"]),
         ],
     )
     def test_infer_accelerators_from_execution_provider(self, execution_providers_test):
         execution_providers, expected_accelerators = execution_providers_test
         actual_rls = AcceleratorLookup.infer_accelerators_from_execution_provider(execution_providers)
+        if actual_rls:
+            actual_rls = sorted(actual_rls)
         assert actual_rls == expected_accelerators
