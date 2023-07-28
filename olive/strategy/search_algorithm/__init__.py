3dc9e542b11e1ac8906945bdea3725d968d8fff2
Emmanuel Assumang <emmanuelassumang@gmail.com>
2023-07-28 11:18:55 +0000

    implemented two algos

--- olive/strategy/search_algorithm/__init__.py
+++ olive/strategy/search_algorithm/__init__.py
@@ -3,6 +3,8 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 from olive.strategy.search_algorithm.exhaustive import ExhaustiveSearchAlgorithm
+from olive.strategy.search_algorithm.nsgaII_sampler import NSGAIISearchAlgorithm
+from olive.strategy.search_algorithm.qmc_sampler import QMCSearchAlgorithm
 from olive.strategy.search_algorithm.random_sampler import RandomSearchAlgorithm
 from olive.strategy.search_algorithm.search_algorithm import SearchAlgorithm
 from olive.strategy.search_algorithm.tpe_sampler import TPESearchAlgorithm
