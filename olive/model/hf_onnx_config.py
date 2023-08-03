5d0d7049e5aed59f72a536f3c146ebdb1c5fd445
Jambay Kinley <jambaykinley@microsoft.com>
2023-08-03 23:07:11 +0000

    simplify

--- olive/model/hf_onnx_config.py
+++ olive/model/hf_onnx_config.py
@@ -1,6 +1,6 @@
 from typing import Any, Mapping, Optional, OrderedDict
 
-from transformers import PreTrainedTokenizer, TensorType, is_torch_available
+from transformers import PreTrainedTokenizer, TensorType
 from transformers.onnx import OnnxConfigWithPast
 
 ADDITIONAL_MODEL_TYPES = {
@@ -89,40 +89,17 @@ class TextDecoderOnnxConfig(OnnxConfigWithPast):
         is_pair: bool = False,
         framework: Optional[TensorType] = None,
     ) -> Mapping[str, Any]:
-        common_inputs = super(OnnxConfigWithPast, self).generate_dummy_inputs(
+        common_inputs = super().generate_dummy_inputs(
             tokenizer, batch_size=batch_size, seq_length=seq_length, is_pair=is_pair, framework=framework
         )
 
         # We need to order the input in the way they appears in the forward()
-        ordered_inputs = OrderedDict({"input_ids": common_inputs["input_ids"]})
-
-        # Need to add the past_keys
+        input_order = ["input_ids"]
         if self.use_past:
-            if not is_torch_available():
-                raise ValueError("Cannot generate dummy past_keys inputs without PyTorch installed.")
-            else:
-                import torch
+            input_order += ["past_key_values"]
+        input_order += ["attention_mask"]
 
-                batch, seqlen = common_inputs["input_ids"].shape
-                # Not using the same length for past_key_values
-                past_key_values_length = seqlen + 2
-                past_shape = (
-                    batch,
-                    self.num_attention_heads,
-                    past_key_values_length,
-                    self._config.hidden_size // self.num_attention_heads,
-                )
-                ordered_inputs["past_key_values"] = [
-                    (torch.zeros(past_shape), torch.zeros(past_shape)) for _ in range(self.num_layers)
-                ]
-
-        ordered_inputs["attention_mask"] = common_inputs["attention_mask"]
-        if self.use_past:
-            mask_dtype = ordered_inputs["attention_mask"].dtype
-            ordered_inputs["attention_mask"] = torch.cat(
-                [ordered_inputs["attention_mask"], torch.ones(batch, past_key_values_length, dtype=mask_dtype)], dim=1
-            )
-
+        ordered_inputs = OrderedDict({k: common_inputs[k] for k in input_order})
         return ordered_inputs
 
     @property
