cc09748d63bb96210e912f3378310b418bbffd1a
trajep <jiapli@microsoft.com>
2023-08-16 11:26:34 +0800

    🟢 Centralize bert CPU e2e aml tests (#491)
    ## Describe your changes
    1. Centralize bert CPU e2e aml tests to reduce confusion.
    2. Re-order the ci job by model type.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- examples/test/test_bert_ptq_cpu.py
+++ examples/test/test_bert_ptq_cpu.py
@@ -21,7 +21,7 @@ def setup():
 
 @pytest.mark.parametrize("search_algorithm", ["tpe"])
 @pytest.mark.parametrize("execution_order", ["joint"])
-@pytest.mark.parametrize("system", ["local_system", "aml_system"])
+@pytest.mark.parametrize("system", ["local_system"])
 @pytest.mark.parametrize("olive_json", ["bert_ptq_cpu.json"])
 def test_bert(search_algorithm, execution_order, system, olive_json):
 
