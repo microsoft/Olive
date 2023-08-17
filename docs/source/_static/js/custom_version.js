a3e651e6c07a0b8d95155b197495560f49e1730d
jiapli <jiapli@microsoft.com>
2023-08-17 11:55:15 +0800

    Auto pull version from git

--- docs/source/_static/js/custom_version.js
+++ docs/source/_static/js/custom_version.js
@@ -4,13 +4,35 @@ $(document).ready(function () {
     // append right arrow to the version
     $(".version").append(" <i class='fa fa-angle-right'></i>");
     $(".version").append(" <i class='fa fa-angle-down' style='display: none;'></i>");
+    // list the version under https://github.com/microsoft/Olive/tree/gh-pages
     var olive_version = {
         "latest": "https://microsoft.github.io/Olive",
-        "0.2.1": "https://microsoft.github.io/Olive/0.2.1/",
-        "0.2.0": "https://microsoft.github.io/Olive/0.2.0/",
-        "0.1.0": "https://microsoft.github.io/Olive/0.1.0/",
-    }
-    for (var version in olive_version) {
+    };
+    $.ajax({
+        url: "https://api.github.com/repos/microsoft/olive/contents?ref=gh-pages",
+        type: "GET",
+        dataType: "json",
+        async: false,
+        success: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var folder_name = data[i].name;
+                // if folder_name is a version number, add it to the version list
+                if (folder_name.match(/^\d+\.\d+\.\d+$/)) {
+                    console.log(folder_name);
+                    olive_version[folder_name] = "https://microsoft.github.io/Olive/" + folder_name + "/";
+                }
+            }
+        }
+    });
+
+    // sort the version list with reverse order
+    var keys = Object.keys(olive_version);
+    keys.sort(function (a, b) {
+        return b.localeCompare(a);
+    });
+
+    for (var i = 0; i < keys.length; i++) {
+        version = keys[i];
         $(".version").append(
             "<div class='to-display-content' style='display: none;'><a class='v_link' href='"
             + olive_version[version] + "'>" + version + "</a></div>");
