8e158d20eab35dc7b10457298b5124850e756279
leqiao <leqiao@microsoft.com>
2023-09-06 14:30:01 +0800

    modify test

--- test/multiple_ep/test_python_env_system.py
+++ test/multiple_ep/test_python_env_system.py
@@ -46,27 +46,3 @@ class TestOliveManagedPythonEnvironmentSystem:
         ]
         assert dml_res[tuple(engine.pass_flows[0])]["metrics"]["latency-avg"]
         assert openvino_res[tuple(engine.pass_flows[0])]["metrics"]["latency-avg"]
-
-    @pytest.mark.skipif(platform.system() == "Windows", reason="Test for Linux only")
-    def test_run_pass_evaluate_linux(self):
-        # use the olive managed python environment as the test environment
-        self.system = PythonEnvironmentSystem(
-            accelerators=["cpu"],
-            olive_managed_env=True,
-        )
-        self.execution_providers = ["CPUExecutionProvider", "OpenVINOExecutionProvider"]
-        temp_dir = tempfile.TemporaryDirectory()
-        output_dir = temp_dir.name
-
-        metric = get_latency_metric(LatencySubType.AVG)
-        evaluator_config = OliveEvaluatorConfig(metrics=[metric])
-        options = {"execution_providers": self.execution_providers}
-        engine = Engine(options, target=self.system, host=self.system, evaluator_config=evaluator_config)
-        engine.register(OrtPerfTuning)
-        output = engine.run(self.input_model, output_dir=output_dir, evaluate_input_model=True)
-        cpu_res = output[AcceleratorSpec(accelerator_type=Device.CPU, execution_provider="CPUExecutionProvider")]
-        openvino_res = output[
-            AcceleratorSpec(accelerator_type=Device.CPU, execution_provider="OpenVINOExecutionProvider")
-        ]
-        assert cpu_res[tuple(engine.pass_flows[0])]["metrics"]["latency-avg"]
-        assert openvino_res[tuple(engine.pass_flows[0])]["metrics"]["latency-avg"]
