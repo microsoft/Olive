2a6e6e891d78a761aeca53b9684f9f2b8333be26
Mike Guo <myguo@microsoft.com>
2023-09-07 18:39:09 +0800

    add hardware accelerator for docker (#546)
    ## Describe your changes
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- olive/systems/docker/eval.py
+++ olive/systems/docker/eval.py
@@ -14,7 +14,7 @@ from olive.model import ModelConfig
 logger = logging.getLogger(__name__)
 
 
-def evaluate_entry(config, output_path, output_name):
+def evaluate_entry(config, output_path, output_name, accelerator_type, execution_provider):
     with open(config, "r") as f:
         config_json = json.load(f)
     evaluator_config = OliveEvaluatorConfig(metrics=config_json["metrics"])
@@ -23,7 +23,9 @@ def evaluate_entry(config, output_path, output_name):
     model = ModelConfig.from_json(model_json).create_model()
 
     evaluator: OliveEvaluator = OliveEvaluatorFactory.create_evaluator_for_model(model)
-    metrics_res = evaluator.evaluate(model, None, evaluator_config.metrics)
+    metrics_res = evaluator.evaluate(
+        model, None, evaluator_config.metrics, device=accelerator_type, execution_providers=execution_provider
+    )
 
     with open(os.path.join(output_path, f"{output_name}"), "w") as f:
         f.write(metrics_res.json())
@@ -37,9 +39,11 @@ if __name__ == "__main__":
     parser.add_argument("--config", type=str, help="evaluation config")
     parser.add_argument("--output_path", help="Path of output model")
     parser.add_argument("--output_name", help="Name of output json file")
+    parser.add_argument("--accelerator_type", type=str, help="accelerator type")
+    parser.add_argument("--execution_provider", type=str, help="execution provider")
 
     args, _ = parser.parse_known_args()
     logger = logging.getLogger("module")
     logger.info("command line arguments: ", sys.argv)
 
-    evaluate_entry(args.config, args.output_path, args.output_name)
+    evaluate_entry(args.config, args.output_path, args.output_name, args.accelerator_type, args.execution_provider)
