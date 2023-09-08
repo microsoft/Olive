829cf3e30f40b6da321f7ec0813ad80c2ec89c94
Jambay Kinley <jambaykinley@microsoft.com>
2023-09-07 22:28:40 -0700

    Use `pass.run` and automatic `tmpdir` in pass unit tests (#549)
    ## Describe your changes
    This is a follow up to the discussion
    https://github.com/microsoft/Olive/pull/545#discussion_r1319314454 to
    simplify the pass unit tests.
    
    `LocalSystem` is not needed to run the pass since we can run the pass
    directly.
    Also use `tmpdir` that is automatically provided by pytest instead of
    creating our own temporary directory.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- test/unit_test/passes/onnx/test_mixed_precision.py
+++ test/unit_test/passes/onnx/test_mixed_precision.py
@@ -1,19 +1,15 @@
-import tempfile
 from pathlib import Path
 from test.unit_test.utils import get_onnx_model
 
 from olive.passes.olive_pass import create_pass_from_dict
 from olive.passes.onnx.mixed_precision import OrtMixedPrecision
-from olive.systems.local import LocalSystem
 
 
-def test_ort_mixed_precision_pass():
+def test_ort_mixed_precision_pass(tmpdir):
     # setup
-    local_system = LocalSystem()
     input_model = get_onnx_model()
     p = create_pass_from_dict(OrtMixedPrecision, {}, disable_search=True)
-    with tempfile.TemporaryDirectory() as tempdir:
-        output_folder = str(Path(tempdir) / "onnx")
+    output_folder = str(Path(tmpdir) / "onnx")
 
-        # execute
-        local_system.run_pass(p, input_model, None, output_folder)
+    # execute
+    p.run(input_model, None, output_folder)
--- test/unit_test/passes/onnx/test_optimum_conversion.py
+++ test/unit_test/passes/onnx/test_optimum_conversion.py
@@ -1,4 +1,3 @@
-import tempfile
 from pathlib import Path
 from test.unit_test.utils import get_optimum_model_by_hf_config, get_optimum_model_by_model_path
 
@@ -6,17 +5,16 @@ import pytest
 
 from olive.passes.olive_pass import create_pass_from_dict
 from olive.passes.onnx.optimum_conversion import OptimumConversion
-from olive.systems.local import LocalSystem
 
 
 @pytest.mark.parametrize("input_model", [get_optimum_model_by_hf_config(), get_optimum_model_by_model_path()])
-def test_optimum_conversion_pass(input_model):
+def test_optimum_conversion_pass(input_model, tmpdir):
     # setup
-    local_system = LocalSystem()
-
     p = create_pass_from_dict(OptimumConversion, {}, disable_search=True)
-    with tempfile.TemporaryDirectory() as tempdir:
-        output_folder = Path(tempdir)
-        onnx_model = local_system.run_pass(p, input_model, None, output_folder)
-        # assert
-        assert Path(onnx_model.model_path).exists()
+    output_folder = Path(tmpdir)
+
+    # execute
+    onnx_model = p.run(input_model, None, output_folder)
+
+    # assert
+    assert Path(onnx_model.model_path).exists()
