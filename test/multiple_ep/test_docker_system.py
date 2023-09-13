7c298755bf389b022f16cdaac8299507b78a298a
Mike Guo <guotuofeng@gmail.com>
2023-09-13 19:16:52 +0800

    update multiple EP test

--- test/multiple_ep/test_docker_system.py
+++ test/multiple_ep/test_docker_system.py
@@ -11,7 +11,7 @@ from olive.engine import Engine
 from olive.evaluator.olive_evaluator import OliveEvaluatorConfig
 from olive.hardware import Device
 from olive.hardware.accelerator import AcceleratorSpec
-from olive.model import ONNXModel
+from olive.model import ModelConfig
 from olive.passes.onnx import OrtPerfTuning
 
 
@@ -27,7 +27,9 @@ class TestOliveManagedDockerSystem:
         self.system = DockerSystem(accelerators=["cpu"], olive_managed_env=True, is_dev=True)
         self.execution_providers = ["CPUExecutionProvider", "OpenVINOExecutionProvider"]
         download_models()
-        self.input_model = ONNXModel(model_path=get_onnx_model())
+        self.input_model_config = ModelConfig.parse_obj(
+            {"type": "ONNXModel", "config": {"model_path": get_onnx_model()}}
+        )
         download_data()
 
     def test_run_pass_evaluate(self):
@@ -41,7 +43,7 @@ class TestOliveManagedDockerSystem:
         options = {"execution_providers": self.execution_providers}
         engine = Engine(options, target=self.system, evaluator_config=evaluator_config)
         engine.register(OrtPerfTuning)
-        output = engine.run(self.input_model, output_dir=output_dir)
+        output = engine.run(self.input_model_config, output_dir=output_dir)
         cpu_res = output[AcceleratorSpec(accelerator_type=Device.CPU, execution_provider="CPUExecutionProvider")]
         openvino_res = output[
             AcceleratorSpec(accelerator_type=Device.CPU, execution_provider="OpenVINOExecutionProvider")
