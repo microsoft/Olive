4f874cf60daeaccab4f899edf9324e70901c84c5
Xiaoyu <85524621+xiaoyu-work@users.noreply.github.com>
2023-09-20 18:02:33 -0700

    update dockerfile (#583)
    ## Describe your changes
    
    * Update dockerfile
    * Add debug info for performance pipeline.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- .azure_pipelines/performance_check/run_performance_check.py
+++ .azure_pipelines/performance_check/run_performance_check.py
@@ -287,6 +287,10 @@ def main():
     if device == "cpu":
         lscpu = subprocess.check_output(["lscpu"])
         print(lscpu.decode("utf-8"))
+        import psutil
+
+        process = [(proc.name(), proc.cpu_percent()) for proc in psutil.process_iter()]
+        print(process)
     elif device == "gpu":
         nvidia_smi = subprocess.check_output(["nvidia-smi"])
         print(nvidia_smi.decode("utf-8"))
