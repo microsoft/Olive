05f4cb9e2fa1c8af4debb5a6977482a5c62d7f96
Xiaoyu <xiaoyuzhang@microsoft.com>
2023-09-21 21:17:07 +0000

    Add missing file

---
+++ olive/utils.py
@@ -0,0 +1,10 @@
+def get_package_name(execution_provider):
+    PROVIDER_PACKAGE_MAPPING = {
+        "CPUExecutionProvider": ("onnxruntime", "ort-nightly"),
+        "CUDAExecutionProvider": ("onnxruntime-gpu", "ort-nightly-gpu"),
+        "TensorrtExecutionProvider": ("onnxruntime-gpu", "ort-nightly-gpu"),
+        "RocmExecutionProvider": ("onnxruntime-gpu", "ort-nightly-gpu"),
+        "OpenVINOExecutionProvider": ("onnxruntime-openvino", None), 
+        "DmlExecutionProvider": ("onnxruntime-directml", "ort-nightly-directml"),
+    }
+    return PROVIDER_PACKAGE_MAPPING.get(execution_provider, ("onnxruntime", "ort-nightly"))
