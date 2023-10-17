5c1d5bc9d05c0b105d72ae9f336be0b4c96c136f
Sheng Xiao <shengxiao0319@126.com>
2023-10-17 11:04:11 +0800

    Update Vitis AI quantization to support ORT 1.16, support TensorData and QuantizationParams

--- olive/passes/onnx/vitis_ai/quantize.py
+++ olive/passes/onnx/vitis_ai/quantize.py
@@ -207,7 +207,20 @@ def quantize_static(
         )
 
         calibrator.collect_data(calibration_data_reader)
-        tensors_range = calibrator.compute_range()
+        try:
+            tensors_range = calibrator.compute_range()
+
+        except AttributeError:
+            tensors_range = calibrator.compute_data()
+            tensors_range_value = {}
+            for key in tensors_range.data.keys():
+                tensors_range_value[key] = tensors_range.data[key].range_value
+            tensors_range = tensors_range_value
+        if not is_ort_version_below_1_16():
+            from onnxruntime.quantization.calibrate import TensorsData
+
+            new_calibrate_tensors_range = TensorsData(CalibrationMethod.MinMax, tensors_range)
+            tensors_range = new_calibrate_tensors_range
         del calibrator
 
     if input_nodes or output_nodes:
--- test/unit_test/passes/vitis_ai/test_vitis_ai_quantization.py
+++ test/unit_test/passes/vitis_ai/test_vitis_ai_quantization.py
@@ -6,10 +6,7 @@ from pathlib import Path
 from test.unit_test.utils import get_onnx_model
 
 import numpy as np
-import pytest
-from onnxruntime import __version__ as OrtVersion
 from onnxruntime.quantization.calibrate import CalibrationDataReader
-from packaging import version
 
 from olive.passes.olive_pass import create_pass_from_dict
 from olive.passes.onnx.vitis_ai_quantization import VitisAIQuantization
@@ -36,10 +33,6 @@ def dummy_calibration_reader(data_dir=None, batch_size=1, *args, **kwargs):
     return RandomDataReader()
 
 
-@pytest.mark.skipif(
-    version.parse(OrtVersion) == version.parse("1.16.0") or version.parse(OrtVersion) == version.parse("1.16.1"),
-    reason="VitisAIQuantization is not supported in ORT 1.16.0 with TensorsData",
-)
 def test_vitis_ai_quantization_pass(tmp_path):
     # setup
     input_model = get_onnx_model()
