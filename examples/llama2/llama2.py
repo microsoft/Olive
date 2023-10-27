5a706fa8b1f4b350f8a13b0a3bde23d8de5d929b
jiapli <jiapli@microsoft.com>
2023-10-27 12:04:53 +0800

    merge template json file

--- examples/llama2/llama2.py
+++ examples/llama2/llama2.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+
 import argparse
 import json
 import re
@@ -11,6 +12,22 @@ from onnxruntime import __version__ as OrtVersion
 from packaging import version
 
 import olive.workflows.run as olive_run
+
+SUPPORTED_WORKFLOWS = {
+    "cpu": [
+        ["conversion_merged", "transformers_optimization_fp32"],
+        ["conversion_merged", "transformers_optimization_fp32", "onnx_dynamic_quant_int8"],
+        ["conversion_merged", "transformers_optimization_fp32", "blockwise_quant_int4"],
+    ],
+    "gpu": [
+        ["conversion_merged", "transformers_optimization_fp16"],
+        ["conversion_merged", "transformers_optimization_fp16", "blockwise_quant_int4"],
+    ],
+}
+DEVICE_TO_EP = {
+    "cpu": "CPUExecutionProvider",
+    "gpu": "CUDAExecutionProvider",
+}
 
 
 def get_args(raw_args):
@@ -28,6 +45,12 @@ def get_args(raw_args):
         required=False,
         help="Whether to use GQA(grouped query attention) instead of MHA(multi-head attention).",
     )
+    parser.add_argument(
+        "--only_config",
+        action="store_true",
+        required=False,
+        help="Whether to use GQA(grouped query attention) instead of MHA(multi-head attention).",
+    )
 
     return parser.parse_args(raw_args)
 
@@ -41,12 +64,7 @@ def main(raw_args=None):
     if not version_1_17:
         raise ValueError("Please use onnxruntime>=1.17.0 for llama2 optimization")
 
-    json_file_template = "llama2_<device>.json"
-    if args.gpu:
-        json_file_template = json_file_template.replace("<device>", "gpu")
-    else:
-        json_file_template = json_file_template.replace("<device>", "cpu")
-
+    json_file_template = "llama2_template.json"
     with open(json_file_template) as f:  # noqa: PTH123
         template_json = json.load(f)
 
@@ -54,11 +72,20 @@ def main(raw_args=None):
     # update model name
     template_json["input_model"]["config"]["hf_config"]["model_name"] = model_name
 
+    # update ep
+    device = "cpu" if not args.gpu else "gpu"
+    template_json["pass_flows"] = SUPPORTED_WORKFLOWS[device]
+
+    template_json["engine"]["execution_providers"] = [DEVICE_TO_EP[device]]
+    template_json["engine"]["output_dir"] = f"llama2_{device}/{model_name}"
+
     if not args.use_gqa and args.gpu:
         template_json["passes"]["transformers_optimization_fp16"]["config"]["use_gqa"] = False
         # after applying GQA, the model's input will be changed, we need to remove the special dataloader implementation
+
         del template_json["passes"]["transformers_optimization_fp16"]["evaluator"]
         del template_json["passes"]["blockwise_quant_int4"]["evaluator"]
+        del template_json["evaluators"]["gqa_evaluator"]
 
     # update user script
     user_script_path = Path(__file__).parent / "user_script.py"
@@ -70,7 +97,8 @@ def main(raw_args=None):
     with open(f"llama2_{device}_{gqa}.json", "w") as f:  # noqa: PTH123
         json.dump(template_json, f, indent=4)
 
-    olive_run(template_json)  # pylint: disable=not-callable
+    if not args.only_config:
+        olive_run(template_json)  # pylint: disable=not-callable
 
 
 def update_user_script(file_path, model_name):
