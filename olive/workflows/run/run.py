d74f0ba433fd9bbb1e63aa3f51645c8dd84cfd64
Mike Guo <myguo@microsoft.com>
2023-11-02 14:05:07 +0800

    Enable ORT Python logging (#684)
    ## Describe your changes
    From the time being, all ORT Python logging is missing except the root
    logger. This PR enable it
    Turn on quant_pre_process to detail logs and let the logging level
    control what should be shown.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Format your code by running `pre-commit run --all-files`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- olive/workflows/run/run.py
+++ olive/workflows/run/run.py
@@ -13,7 +13,7 @@ from typing import List, Union
 import onnxruntime as ort
 
 from olive.hardware import Device
-from olive.logging import set_default_logger_severity, set_verbosity_info
+from olive.logging import set_default_logger_severity, set_ort_logger_severity, set_verbosity_info
 from olive.passes import Pass
 from olive.systems.common import SystemType
 from olive.workflows.run.config import RunConfig
@@ -142,6 +142,10 @@ def run(config: Union[str, Path, dict], setup: bool = False, data_root: str = No
 
     # set ort log level
     set_default_logger_severity(config.engine.log_severity_level)
+    # the ort_log_severity_level is used to control the C++ logging levels.
+    # As the result, we use the Olive logger level to control the ORT Python logging level.
+    # TODO(myguo): do we need use another config to control the ORT Python logging?
+    set_ort_logger_severity(config.engine.log_severity_level)
     ort.set_default_logger_severity(config.engine.ort_log_severity_level)
 
     # input model
