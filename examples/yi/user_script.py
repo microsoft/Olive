b7e28b495e398160af727da943d58c3e3e3d270c
Xiaoyu <xiaoyuzhang@microsoft.com>
2023-11-10 00:58:41 +0000

    fix format

--- examples/yi/user_script.py
+++ examples/yi/user_script.py
@@ -1,10 +1,10 @@
 import torch
 
+
 def dummy_inputs(model):
     attention_mask_sequence_length = 19
     sequence_length = 19
-    inputs = {
+    return {
         "input_ids": torch.randint(10, (1, sequence_length), dtype=torch.int64),
         "attention_mask": torch.randint(10, (1, attention_mask_sequence_length), dtype=torch.int64),
     }
-    return inputs
