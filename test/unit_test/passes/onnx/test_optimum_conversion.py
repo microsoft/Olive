bd629e69393bd57bcaba3bc530c09112eb3b6a2f
trajep <jiapli@microsoft.com>
2023-11-22 17:55:51 +0800

    🌵 Complete optimum conversion/merge configs (#739)
    ## Describe your changes
    
    Complete optimum conversion/merge configs.
    Take conversion as example, the `fp16=True` means to load the torch
    model with float16 from huggingface model hub. Then the fp16 torch model
    is converted to onnx model.
    That is different the fp16 in transformer optimization.
    
    ## Checklist before requesting a review
    - [x] Add unit tests for this change.
    - [x] Make sure all tests can pass.
    - [x] Update documents if necessary.
    - [x] Lint and apply fixes to your code by running `lintrunner -a`
    - [x] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- test/unit_test/passes/onnx/test_optimum_conversion.py
+++ test/unit_test/passes/onnx/test_optimum_conversion.py
@@ -12,10 +12,13 @@ from olive.passes.olive_pass import create_pass_from_dict
 from olive.passes.onnx.optimum_conversion import OptimumConversion
 
 
-@pytest.mark.parametrize("input_model", [get_optimum_model_by_hf_config(), get_optimum_model_by_model_path()])
-def test_optimum_conversion_pass(input_model, tmp_path):
+@pytest.mark.parametrize(
+    "input_model,extra_args",
+    [(get_optimum_model_by_hf_config(), {"atol": 0.1}), (get_optimum_model_by_model_path(), {"atol": None})],
+)
+def test_optimum_conversion_pass(input_model, extra_args, tmp_path):
     # setup
-    p = create_pass_from_dict(OptimumConversion, {}, disable_search=True)
+    p = create_pass_from_dict(OptimumConversion, {"extra_args": extra_args}, disable_search=True)
     output_folder = tmp_path
 
     # execute
@@ -23,3 +26,30 @@ def test_optimum_conversion_pass(input_model, tmp_path):
 
     # assert
     assert Path(onnx_model.model_path).exists()
+
+
+@pytest.mark.parametrize(
+    "config,is_valid",
+    [
+        ({"fp16": True}, False),
+        ({"fp16": True, "device": "cpu"}, False),
+        ({"fp16": False, "device": "cpu"}, True),
+    ],
+)
+def test_optimum_configs(config, is_valid, tmp_path):
+    input_model = get_optimum_model_by_hf_config()
+    # setup
+    p = create_pass_from_dict(OptimumConversion, config, disable_search=True)
+    output_folder = tmp_path
+
+    if not is_valid:
+        assert p.validate_search_point(config, None) is False
+        with pytest.raises(
+            ValueError,
+            match="FP16 export is supported only when exporting on GPU. Please pass the option `--device cuda`.",
+        ):
+            p.run(input_model, None, output_folder)
+    else:
+        assert p.validate_search_point(config, None)
+        onnx_model = p.run(input_model, None, output_folder)
+        assert Path(onnx_model.model_path).exists()
