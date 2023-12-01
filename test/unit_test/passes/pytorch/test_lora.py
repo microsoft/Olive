c29b87c3f872c9f88e1c96119635f2765a5c406c
Jambay Kinley <jambaykinley@microsoft.com>
2023-12-01 00:48:06 -0800

    LoRA/QLoRA: Add `bfloat16` support for ort-training (#769)
    ## Describe your changes
    ONNX Runtime training now supports `bfloat16` for lora and qlora.
    
    This PR
    - Enables ort-training + bfloat16 by setting
    `ORTMODULE_ONNX_OPSET_VERSION` environment variable. Default value is 16
    which is required for models with operators such as Where.
    - Adds unit test for the environment variable.
    - Improves dependency checks in the lora passes.
    - Better organizes the extras for lora/qlora. Workflow setup allows
    assigning multiple extras to a single pass.
    
    ## Checklist before requesting a review
    - [x] Add unit tests for this change.
    - [x] Make sure all tests can pass.
    - [x] Update documents if necessary.
    - [x] Lint and apply fixes to your code by running `lintrunner -a`
    - [x] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    LoRA/QLoRA support `bfloat16` with ort-training.
    ## (Optional) Issue link

--- test/unit_test/passes/pytorch/test_lora.py
+++ test/unit_test/passes/pytorch/test_lora.py
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+import logging
+import os
 import sys
 from pathlib import Path
 from unittest.mock import MagicMock, patch
@@ -20,8 +22,8 @@ def patched_find_submodules(*args, **kwargs):
     return ["k_proj", "v_proj", "out_proj", "q_proj", "fc1", "fc2"]
 
 
-def get_dataset():
-    return {
+def get_pass_config(model_name, task, **kwargs):
+    dataset = {
         "data_name": "ptb_text_only",
         "subset": "penn_treebank",
         "split": "train",
@@ -35,9 +37,6 @@ def get_dataset():
             }
         },
     }
-
-
-def get_pass_config(model_name, task, **dataset):
     data_config = huggingface_data_config_template(model_name=model_name, task=task, **dataset).to_json()
     return {
         "train_data_config": data_config,
@@ -50,22 +49,27 @@ def get_pass_config(model_name, task, **dataset):
             "logging_steps": 1,
             "optim": "adamw_hf",
         },
+        **kwargs,
     }
 
 
-def test_lora(tmp_path):
+def run_finetuning(pass_class, tmp_path, **pass_config_kwargs):
     # setup
     model_name = "hf-internal-testing/tiny-random-OPTForCausalLM"
     task = "text-generation"
     input_model = PyTorchModel(hf_config={"model_name": model_name, "task": task})
-    dataset = get_dataset()
     # convert to json to ensure the pass can handle serialized data config
-    config = get_pass_config(model_name, task, **dataset)
-    p = create_pass_from_dict(LoRA, config, disable_search=True)
-    output_folder = str(tmp_path / "lora")
+    config = get_pass_config(model_name, task, **pass_config_kwargs)
+    p = create_pass_from_dict(pass_class, config, disable_search=True)
+    output_folder = str(tmp_path / "output_model")
 
     # execute
-    out = p.run(input_model, None, output_folder)
+    return p.run(input_model, None, output_folder)
+
+
+def test_lora(tmp_path):
+    # execute
+    out = run_finetuning(LoRA, tmp_path)
 
     # assert
     assert Path(out.get_resource("adapter_path")).exists()
@@ -85,18 +89,52 @@ def mock_bitsandbytes_fixture():
 @patch("olive.passes.pytorch.lora.HFModelLoadingArgs")
 @patch("olive.passes.pytorch.lora.find_submodules", side_effect=patched_find_submodules)
 def test_qlora(patched_model_loading_args, patched_find_submodules, tmp_path, mock_bitsandbytes):
-    # setup
-    model_name = "hf-internal-testing/tiny-random-OPTForCausalLM"
-    task = "text-generation"
-    input_model = PyTorchModel(hf_config={"model_name": model_name, "task": task})
-    dataset = get_dataset()
-    # convert to json to ensure the pass can handle serialized data config
-    config = get_pass_config(model_name, task, **dataset)
-    p = create_pass_from_dict(QLoRA, config, disable_search=True)
-    output_folder = str(tmp_path / "qlora")
-
     # execute
-    out = p.run(input_model, None, output_folder)
+    out = run_finetuning(QLoRA, tmp_path)
 
     # assert
     assert Path(out.get_resource("adapter_path")).exists()
+
+
+@pytest.fixture(name="mock_torch_ort")
+def mock_torch_ort_fixture():
+    # mock torch_ort since we don't install it in the test environment
+    mock_torch_ort = MagicMock()
+    sys.modules["torch_ort"] = mock_torch_ort
+    yield mock_torch_ort
+    del sys.modules["torch_ort"]
+
+
+@pytest.fixture(name="clean_env")
+def clean_env_fixture():
+    yield
+    if "ORTMODULE_ONNX_OPSET_VERSION" in os.environ:
+        del os.environ["ORTMODULE_ONNX_OPSET_VERSION"]
+
+
+@pytest.mark.usefixtures("clean_env", "mock_torch_ort")
+@pytest.mark.parametrize("value,expected_value", [(None, "16"), (-1, None), (16, "16"), (15, "15"), (17, "17")])
+@patch("olive.passes.pytorch.lora.LoRA.train_and_save_new_model")
+@patch("optimum.onnxruntime.utils.is_onnxruntime_training_available", return_value=True)
+@patch("onnxruntime.__version__", "1.17.0")
+def test_ortmodule_onnx_opset_version(_, tmp_path, caplog, value, expected_value):
+    # execute
+    pass_config_kwargs = {"use_ort_trainer": True}
+    if value is not None:
+        pass_config_kwargs["ortmodule_onnx_opset_version"] = value
+
+    if expected_value is None:
+        # invalid value
+        with pytest.raises(AssertionError):
+            run_finetuning(LoRA, tmp_path, **pass_config_kwargs)
+    else:
+        # capture logging to check for opset < 16 warning
+        logger = logging.getLogger("olive")
+        logger.propagate = True
+
+        run_finetuning(LoRA, tmp_path, **pass_config_kwargs)
+
+        # assert
+        assert os.environ["ORTMODULE_ONNX_OPSET_VERSION"] == expected_value
+        if int(expected_value) < 16:
+            assert "training with bfloat16 might not work properly" in caplog.text
