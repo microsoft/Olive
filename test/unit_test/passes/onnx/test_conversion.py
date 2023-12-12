423c6f7ae84d79cde895a6aba3e8bfee81c95802
Jambay Kinley <jambaykinley@microsoft.com>
2023-12-12 00:53:36 +0000

    add unit test

--- test/unit_test/passes/onnx/test_conversion.py
+++ test/unit_test/passes/onnx/test_conversion.py
@@ -5,9 +5,11 @@
 
 from pathlib import Path
 from test.unit_test.utils import get_hf_model_with_past, get_onnx_model, get_pytorch_model
+from unittest.mock import patch
 
 import pytest
 
+from olive.model import PyTorchModelHandler
 from olive.passes.olive_pass import create_pass_from_dict
 from olive.passes.onnx.conversion import OnnxConversion, OnnxOpVersionConversion
 
@@ -24,6 +26,35 @@ def test_onnx_conversion_pass(input_model, tmp_path):
 
     # assert
     assert Path(onnx_model.model_path).exists()
+
+
+# TODO(jambayk): add case for when quantized_modules is not provided when gpu test is added
+# patch needed since bitsandbytes is not installed in the test environment
+@patch("olive.model.config.hf_config.HfFromPretrainedArgs.get_quantization_config")
+def test_onnx_conversion_pass_quant_model(_, tmp_path):
+    # setup
+    quantized_modules = ["v_proj", "k_proj", "fc_in", "fc_out", "out_proj", "q_proj"]
+    input_model = PyTorchModelHandler(
+        hf_config={
+            "model_name": "hf-internal-testing/tiny-random-gptj",
+            "task": "text-generation",
+            "from_pretrained_args": {
+                "quantization_method": "bitsandbytes",
+                "quantization_config": {"load_in_4bit": True, "bnb_4bit_quant_type": "nf4"},
+            },
+        },
+        model_attributes={"quantized_modules": quantized_modules},
+    )
+    p = create_pass_from_dict(OnnxConversion, {}, disable_search=True)
+    output_folder = str(tmp_path / "onnx")
+
+    onnx_model = p.run(input_model, None, output_folder)
+
+    # assert
+    assert Path(onnx_model.model_path).exists()
+    assert set(onnx_model.model_attributes["quantized_modules"]) == set(quantized_modules)
+    assert onnx_model.model_attributes["quantization_config"]["load_in_4bit"] is True
+    assert onnx_model.model_attributes["quantization_config"]["bnb_4bit_quant_type"] == "nf4"
 
 
 @pytest.mark.parametrize("target_opset", [9, 10, 16])
