93754767a7262a17656aec92be4407e95d8f0106
Patrice Vignola <vignola.patrice@gmail.com>
2023-12-11 18:08:24 -0800

    Onboard sdxl turbo to the DirectML examples

--- examples/directml/stable_diffusion_xl/config.py
+++ examples/directml/stable_diffusion_xl/config.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 
-image_size = 1024
-hidden_state_size = 2048
+vae_sample_size = 1024
+unet_sample_size = 128
+cross_attention_dim = 2048
 time_ids_size = 6
