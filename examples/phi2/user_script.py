844ca290cec4814c074cb3859817d74f30bbb098
Mike Guo <myguo@microsoft.com>
2024-01-12 15:23:19 +0800

    add transformer optimization

--- examples/phi2/user_script.py
+++ examples/phi2/user_script.py
@@ -165,13 +165,13 @@ def get_merged_sample_with_past_kv_inputs(
     input_ids = torch.randint(low=0, high=config.vocab_size, size=(batch_size, seq_len), dtype=torch.int64)
     attention_mask = torch.ones(batch_size, past_seq_len + seq_len, dtype=torch.int64)
     # position_ids is of shape (batch_size, seq_len) for prompt generation, (batch_size, 1) for token generation
-    # position_ids = get_position_ids(attention_mask, use_past_kv=(past_seq_len != 0))
+    position_ids = get_position_ids(attention_mask, past_seq_len)
     past_kv = get_past_kv_inputs(config, batch_size, past_seq_len, use_fp16, world_size=world_size)
 
     # Convert inputs to NumPy (for ORT) or send to device (for PyTorch)
     input_ids = input_ids.numpy() if engine == "ort" else input_ids.to(device)
     attention_mask = attention_mask.numpy() if engine == "ort" else attention_mask.to(device)
-    # position_ids = position_ids.numpy() if engine == "ort" else position_ids.to(device)
+    position_ids = position_ids.numpy() if engine == "ort" else position_ids.to(device)
 
     # ruff: noqa: C417
     past_kv = (
@@ -183,11 +183,12 @@ def get_merged_sample_with_past_kv_inputs(
     if not return_dict:
         # For export
         assert isinstance(past_kv, list)
-        return (input_ids, past_kv, attention_mask)
+        return (input_ids, past_kv, attention_mask, position_ids)
 
     inputs = {
         "input_ids": input_ids,
         "attention_mask": attention_mask,
+        "position_ids": position_ids,
     }
     if engine == "ort":
         assert isinstance(past_kv, dict)
