7c4fed91f287ea41837583ca1e2bcab7b55b5322
Mike Guo <myguo@microsoft.com>
2024-01-12 11:34:49 +0800

    update phi2 example corresponding to the latest phi2 model code

--- examples/phi2/user_script.py
+++ examples/phi2/user_script.py
@@ -1,14 +1,12 @@
 # -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-# --------------------------------------------------------------------------
-from dataclasses import dataclass, field
 from itertools import chain
-from typing import TYPE_CHECKING, Any, Dict, List, Tuple
+from typing import TYPE_CHECKING, List, Tuple
 
 import numpy as np
 import torch
-from transformers import AutoConfig, AutoModelForCausalLM
+from transformers import AutoConfig
 
 from olive.constants import Framework
 
@@ -19,78 +17,9 @@ model_id = "microsoft/phi-2"
 config: "PhiConfig" = AutoConfig.from_pretrained(model_id, trust_remote_code=True)
 
 
-# TODO(myguo): the following code is copied from
-# https://huggingface.co/microsoft/phi-2/blob/main/modeling_phi.py
-# need remove the definition and forward patch logic after the official phi2 support the list of past_key_values
-@dataclass
-class InferenceParams:
-    """Inference parameters passed to model to efficiently calculate and store context during inference.
-
-    Reference:
-        https://github.com/Dao-AILab/flash-attention/blob/main/flash_attn/utils/generation.py.
-
-    Args:
-        max_seqlen: Maximum sequence length.
-        max_batch_size: Maximum batch size.
-        seqlen_offset: Sequence length offset.
-        batch_size_offset: Batch size offset.
-        key_value_memory_dict: Key value memory dictionary.
-        lengths_per_sample: Lengths per sample.
-    """
-
-    max_seqlen: int = field(metadata={"help": "Maximum sequence length."})
-
-    max_batch_size: int = field(metadata={"help": "Maximum batch size."})
-
-    seqlen_offset: int = field(default=0, metadata={"help": "Sequence length offset."})
-
-    batch_size_offset: int = field(default=0, metadata={"help": "Batch size offset."})
-
-    key_value_memory_dict: Dict[str, Any] = field(
-        default_factory=dict, metadata={"help": "Key value memory dictionary."}
-    )
-
-    lengths_per_sample: torch.Tensor = field(default=None, metadata={"help": "Lengths per sample."})
-
-
-def post_process(result, num_layer):
-    present = []
-    for i in range(num_layer):
-        present.append(result.past_key_values.key_value_memory_dict[i])
-    return (result.logits, tuple(present))
-
-
-def patched_forward(forward):
-    def inner_forward(input_ids, attention_mask, past_key_values):
-        kv_mem_dict = {}
-        for i, kv in enumerate(past_key_values):
-            kv_mem_dict[i] = kv
-
-        inference_params = InferenceParams(
-            max_seqlen=config.n_positions,
-            max_batch_size=input_ids.shape[0],
-            seqlen_offset=0,
-            batch_size_offset=0,
-            key_value_memory_dict=kv_mem_dict,
-            lengths_per_sample=None,
-        )
-        result = forward(input_ids, attention_mask=attention_mask, past_key_values=inference_params)
-        return post_process(result, config.num_hidden_layers)
-
-    return inner_forward
-
-
-def model_loader(model_path):
-    model = AutoModelForCausalLM.from_pretrained(
-        "microsoft/phi-2", torch_dtype=torch.float32, device_map="cpu", trust_remote_code=True
-    )
-    model.forward = patched_forward(model.forward)
-    return model
-
-
 def dummy_inputs(model):
     """Get dummy inputs for merged decoder model with past_key_values."""
-    batch_size, sequence_length, past_sequence_length = 2, 8, 0
+    batch_size, sequence_length, past_sequence_length = 2, 8, config.num_hidden_layers
     max_sequence_length = 512
 
     return get_merged_sample_with_past_kv_inputs(
@@ -112,11 +41,15 @@ def get_io_config(model):
     input_names = [
         "input_ids",
         "attention_mask",
-        *list(chain.from_iterable((f"past_key_values.{i}",) for i in range(config.num_hidden_layers))),
+        *list(
+            chain.from_iterable(
+                (f"past_key_values.{i}.key", f"past_key_values.{i}.value") for i in range(config.num_hidden_layers)
+            )
+        ),
     ]
     output_names = [
         "logits",
-        *list(chain.from_iterable((f"present_key_values.{i}",) for i in range(config.num_hidden_layers))),
+        *list(chain.from_iterable((f"present.{i}.key", f"present.{i}.value") for i in range(config.num_hidden_layers))),
     ]
     dynamic_axes = get_merged_model_dynamic_axes(input_names, output_names)
     return {
@@ -127,7 +60,7 @@ def get_io_config(model):
 
 
 def create_dataloader(data_dir, batch_size, *args, **kwargs):
-    sequence_length, past_sequence_length = 8, 0
+    sequence_length, past_sequence_length = 8, config.num_hidden_layers
     max_sequence_length = 512
     model_framework = kwargs.get("model_framework", Framework.PYTORCH)
     engine = "ort" if model_framework == Framework.ONNX else "pt"
@@ -233,7 +166,7 @@ def get_merged_sample_with_past_kv_inputs(
     attention_mask = torch.ones(batch_size, past_seq_len + seq_len, dtype=torch.int64)
     # position_ids is of shape (batch_size, seq_len) for prompt generation, (batch_size, 1) for token generation
     # position_ids = get_position_ids(attention_mask, use_past_kv=(past_seq_len != 0))
-    past_kv = get_past_kv_inputs(config, batch_size, max_seq_len, use_fp16, world_size=world_size)
+    past_kv = get_past_kv_inputs(config, batch_size, past_seq_len, use_fp16, world_size=world_size)
 
     # Convert inputs to NumPy (for ORT) or send to device (for PyTorch)
     input_ids = input_ids.numpy() if engine == "ort" else input_ids.to(device)
@@ -241,7 +174,11 @@ def get_merged_sample_with_past_kv_inputs(
     # position_ids = position_ids.numpy() if engine == "ort" else position_ids.to(device)
 
     # ruff: noqa: C417
-    past_kv = flatten_past_kv_inputs(past_kv) if engine == "ort" else list(map(lambda kv: (kv[0].to(device)), past_kv))
+    past_kv = (
+        flatten_past_kv_inputs(past_kv)
+        if engine == "ort"
+        else list(map(lambda kv: (kv[0].to(device), kv[1].to(device)), past_kv))
+    )
 
     if not return_dict:
         # For export
@@ -270,8 +207,9 @@ def flatten_past_kv_inputs(past_key_values: List[Tuple[torch.Tensor, torch.Tenso
     """Flatten past_key_values to a dict of past_key and past_value. For ONNX model only."""
     past_kv = {}
     # Convert list of past_kv to dict of past_key and past_value
-    for i, value in enumerate(past_key_values):
-        past_kv[f"past_key_values.{i}"] = value[0].numpy()
+    for i, (past_k, past_v) in enumerate(past_key_values):
+        past_kv[f"past_key_values.{i}.key"] = past_k
+        past_kv[f"past_key_values.{i}.value"] = past_v
     return past_kv
 
 
@@ -288,9 +226,13 @@ def enable_past_present_share_buffer(ort_inputs: dict, past_seq_len: int, max_se
 
 
 def get_past_kv_inputs(config: AutoConfig, batch_size: int, past_seq_len: int, use_fp16: bool, world_size: int = 1):
-    num_heads, head_size = config.n_head // world_size, config.n_embd // config.n_head
+    num_heads = config.num_attention_heads // world_size
+    head_size = config.hidden_size // config.num_attention_heads
     torch_dtype = torch.float16 if use_fp16 else torch.float32
     return [
-        (torch.rand(batch_size, past_seq_len, 2, num_heads, head_size, dtype=torch_dtype),)
+        (
+            torch.rand(batch_size, num_heads, past_seq_len, head_size, dtype=torch_dtype),
+            torch.rand(batch_size, num_heads, past_seq_len, head_size, dtype=torch_dtype),
+        )
         for _ in range(config.num_hidden_layers)
     ]
