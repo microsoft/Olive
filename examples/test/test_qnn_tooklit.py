8c017ada5dde410a8f1f36a9b8e5a107398ddb3f
trajepl <jiapli@microsoft.com>
2024-01-23 17:22:27 +0800

    Enable windows qnn e2e test

--- examples/test/test_qnn_tooklit.py
+++ examples/test/test_qnn_tooklit.py
@@ -3,6 +3,8 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 import os
+import platform
+import tempfile
 from pathlib import Path
 
 import pytest
@@ -11,35 +13,56 @@ from utils import check_output, download_azure_blob
 from olive.common.utils import retry_func, run_subprocess
 
 
-def download_qnn_sdk():
+def download_qnn_sdk(target_path=None):
     """Download the qnn sdk."""
+    blob, download_path = "", ""
+    if platform.system() == "Windows":
+        blob, download_path = "qnn_sdk_windows.zip", "qnn_sdk_windows.zip"
+    elif platform.system() == "Linux":
+        blob, download_path = "qnn_sdk_linux.zip", "qnn_sdk_linux.zip"
+
     download_azure_blob(
         container="olivetest",
-        blob="qnn_sdk_linux.zip",
-        download_path="qnn_sdk_linux.zip",
+        blob=blob,
+        download_path=download_path,
     )
-    target_path = Path().resolve()
-    run_subprocess(cmd=f"unzip qnn_sdk_linux.zip -d {str(target_path)}", check=True)
+    if not target_path:
+        target_path = Path().resolve() / "qnn_sdk"
+    target_path.mkdir(parents=True, exist_ok=True)
+    if platform.system() == "Windows":
+        import zipfile
+
+        with zipfile.ZipFile(download_path, "r") as zip_ref:
+            zip_ref.extractall(target_path)
+    elif platform.system() == "Linux":
+        run_subprocess(cmd=f"unzip {download_path} -d {str(target_path)}", check=True)
     return target_path
 
 
 @pytest.fixture(scope="module", autouse=True)
 def setup():
     """Setups any state specific to the execution of the given module."""
+    tmp_dir = tempfile.TemporaryDirectory()
+    tmp_path = Path(tmp_dir.name)
     cur_dir = Path(__file__).resolve().parent.parent
     example_dir = cur_dir / "mobilenet_qnn_qualcomm_npu"
     os.chdir(example_dir)
 
     # prepare model and data
     # retry since it fails randomly
-    os.environ["QNN_SDK_ROOT"] = str(download_qnn_sdk() / "opt" / "qcom" / "aistack")
+    os.environ["QNN_SDK_ROOT"] = str(download_qnn_sdk(tmp_path) / "opt" / "qcom" / "aistack")
     run_subprocess(cmd="python -m olive.platform_sdk.qualcomm.configure --py_version 3.8 --sdk qnn", check=True)
     # install dependencies
+    python_cmd = ""
+    if platform.system() == "Windows":
+        python_cmd = str(Path(os.environ["QNN_SDK_ROOT"]) / "olive-pyenv" / "python.exe")
+    elif platform.system() == "Linux":
+        python_cmd = str(Path(os.environ["QNN_SDK_ROOT"]) / "olive-pyenv" / "bin" / "python")
     install_cmd = [
-        str(Path(os.environ["QNN_SDK_ROOT"]) / "olive-pyenv" / "bin" / "python"),
+        python_cmd,
         str(Path(os.environ["QNN_SDK_ROOT"]) / "bin" / "check-python-dependency"),
     ]
-    run_subprocess(cmd="\n".join(install_cmd), check=True)
+    run_subprocess(cmd=" ".join(install_cmd), check=True)
     retry_func(run_subprocess, kwargs={"cmd": "python download_files.py", "check": True})
     retry_func(run_subprocess, kwargs={"cmd": "python prepare_config.py --use_raw_qnn_sdk", "check": True})
     yield
