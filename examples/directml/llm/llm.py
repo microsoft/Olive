b271ea54053834ecab7492b4a40f7e6c05b06851
Aarushi Majumder <aamajumder@microsoft.com>
2024-01-30 18:19:22 -0800

    lint error fix

--- examples/directml/llm/llm.py
+++ examples/directml/llm/llm.py
@@ -210,7 +210,14 @@ if __name__ == "__main__":
     parser.add_argument(
         "--model_type",
         default="llama-2-7b-chat",
-        choices=["llama-2-7b-chat", "mistral-7b-chat", "falcon-7b-chat", "mistral-7b-openorca", "codellama-7b-chat", "llava-7b"],
+        choices=[
+            "llama-2-7b-chat",
+            "mistral-7b-chat",
+            "falcon-7b-chat",
+            "mistral-7b-openorca",
+            "codellama-7b-chat",
+            "llava-7b",
+        ],
         help="Which model to convert.",
         type=str,
     )
