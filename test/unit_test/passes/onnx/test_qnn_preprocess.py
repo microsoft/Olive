5ca79b61c4324ae02918e5677acbf99b12162cc5
trajepl <jiapli@microsoft.com>
2024-02-29 20:41:04 +0800

    fix

--- test/unit_test/passes/onnx/test_qnn_preprocess.py
+++ test/unit_test/passes/onnx/test_qnn_preprocess.py
@@ -14,11 +14,10 @@ from olive.passes.onnx import QNNPreprocess
     version.parse(OrtVersion) < version.parse("1.17.0"),
     reason="qnn quantization is only supported in onnxruntime>=1.17.0",
 )
-def test_qnn_preprocess(tmp_path):
+def test_qnn_preprocess_not_change_model(tmp_path):
     input_model = get_onnx_model()
     p = create_pass_from_dict(QNNPreprocess, {}, disable_search=True)
     out = p.run(input_model, None, tmp_path)
-    assert out is not None
     assert out == input_model
 
 
@@ -33,5 +32,4 @@ def test_qnn_preprocess_changed_model(mocked_qnn_preprocess_model, tmp_path):
     shutil.copy(input_model.model_path, tmp_path / "dummy_model.onnx")
     p = create_pass_from_dict(QNNPreprocess, {}, disable_search=True)
     out = p.run(input_model, None, tmp_path)
-    assert out is not None
     assert out != input_model
