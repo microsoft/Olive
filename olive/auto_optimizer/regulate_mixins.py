1cd496836f3db73b536a5bc1260672d38e6f5bdd
jiapli <jiapli@microsoft.com>
2024-03-13 14:18:52 +0800

    merge main

--- olive/auto_optimizer/regulate_mixins.py
+++ olive/auto_optimizer/regulate_mixins.py
@@ -37,6 +37,10 @@ class RegulatePassConfigMixin:
                     if p not in pass_config:
                         pass_config.update({p: {"type": p, "config": {}}})
 
+        # disable pass search when search strategy is None/False
+        if not self.evaluator_config:
+            for pass_name in pass_config:
+                pass_config[pass_name]["disable_search"] = True
         return pass_config, pass_flows
 
     def _regulate_fp16(self, pass_config, pass_flows):
