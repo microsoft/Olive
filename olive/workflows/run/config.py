a67398a66d7237c3be12ec336124eceafdfa54b8
trajep <jiapli@microsoft.com>
2024-03-21 10:39:37 +0800

    🅰️ AutoOpt disable search when no evaluator (#1013)
    ## Describe your changes
    This PR is used for:
    1. If there no passes given, enable AutoOpt be default.
    2. If the evaluation config is given, in AutoOpt case, we should set
    disable_search as True.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    - [ ] Is this PR including examples changes? If yes, please remember to
    update [example
    documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
    in a follow-up PR.
    
    ## (Optional) Issue link

--- olive/workflows/run/config.py
+++ olive/workflows/run/config.py
@@ -248,6 +248,12 @@ class RunConfig(ConfigBase):
                 )
         return v
 
+    @validator("auto_optimizer_config", always=True)
+    def validate_auto_optimizer_config(cls, v, values):
+        if not v:
+            v = AutoOptimizerConfig()
+        return v
+
 
 def _resolve_config_str(v, values, alias, component_name):
     """Resolve string value for alias in v to corresponding component config in values.
