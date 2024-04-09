036ac024324cbdb99c6e582d3a2c02aa98f360b4
Mike Guo <guotuofeng@gmail.com>
2024-04-09 10:28:06 +0800

    Only turn off azure logging when user doesn't specify them

--- olive/azureml/azureml_client.py
+++ olive/azureml/azureml_client.py
@@ -80,9 +80,7 @@ class AzureMLClientConfig(ConfigBase):
         """Create an MLClient instance."""
         from azure.ai.ml import MLClient
 
-        # set logger level to error to avoid too many logs from azure sdk
-        logging.getLogger("azure.ai.ml").setLevel(logging.ERROR)
-        logging.getLogger("azure.identity").setLevel(logging.ERROR)
+        set_azure_logging_if_noset()
 
         if self.aml_config_path is None:
             if self.subscription_id is None:
@@ -109,9 +107,7 @@ class AzureMLClientConfig(ConfigBase):
         """Create an MLClient instance."""
         from azure.ai.ml import MLClient
 
-        # set logger level to error to avoid too many logs from azure sdk
-        logging.getLogger("azure.ai.ml").setLevel(logging.ERROR)
-        logging.getLogger("azure.identity").setLevel(logging.ERROR)
+        set_azure_logging_if_noset()
 
         return MLClient(credential=self._get_credentials(), registry_name=registry_name)
 
@@ -138,3 +134,15 @@ class AzureMLClientConfig(ConfigBase):
             credential = InteractiveBrowserCredential()
 
         return credential
+
+
+def set_azure_logging_if_noset():
+    # set logger level to error to avoid too many logs from azure sdk
+    azure_ml_logger = logging.getLogger("azure.ai.ml")
+    # only set the level if it is not set, to avoid changing the level set by the user
+    if not azure_ml_logger.level:
+        azure_ml_logger.setLevel(logging.ERROR)
+    azure_identity_logger = logging.getLogger("azure.identity")
+    # only set the level if it is not set, to avoid changing the level set by the user
+    if not azure_identity_logger.level:
+        azure_identity_logger.setLevel(logging.ERROR)
