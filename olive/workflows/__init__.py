cd5637082e88fb0f987c42d814100c0e814df191
Jambay Kinley <jambaykinley@microsoft.com>
2024-04-10 16:56:02 -0700

    merger run_impl and run

--- olive/workflows/__init__.py
+++ olive/workflows/__init__.py
@@ -2,6 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
-from olive.workflows.run_impl import run
+from olive.workflows.run import run
 
 __all__ = ["run"]
