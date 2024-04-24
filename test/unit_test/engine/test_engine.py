cc4b3428ea6c76832ce547911929dd96836fa1c8
Mike Guo <myguo@microsoft.com>
2024-04-24 14:42:59 +0800

    Codeql: fix circular import betweeen accelerator and system_config (#1104)
    ## Describe your changes
    * Fix codeql
    https://github.com/microsoft/Olive/security/code-scanning/10641
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    - [ ] Is this PR including examples changes? If yes, please remember to
    update [example
    documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
    in a follow-up PR.
    
    ## (Optional) Issue link

--- test/unit_test/engine/test_engine.py
+++ test/unit_test/engine/test_engine.py
@@ -22,10 +22,10 @@ from olive.evaluator.metric import AccuracySubType
 from olive.evaluator.metric_result import MetricResult, joint_metric_key
 from olive.evaluator.olive_evaluator import OliveEvaluatorConfig
 from olive.hardware import DEFAULT_CPU_ACCELERATOR
-from olive.hardware.accelerator import create_accelerators
 from olive.passes.onnx.conversion import OnnxConversion
 from olive.passes.onnx.optimum_conversion import OptimumConversion
 from olive.passes.onnx.quantization import OnnxDynamicQuantization, OnnxStaticQuantization
+from olive.systems.accelerator_creator import create_accelerators
 from olive.systems.common import SystemType
 from olive.systems.local import LocalSystem
 from olive.systems.system_config import LocalTargetUserConfig, SystemConfig
