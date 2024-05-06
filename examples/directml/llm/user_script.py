63c0430fe3da033e086e1dfbe00d9bdc918ecca8
Patrice Vignola <vignola.patrice@gmail.com>
2024-05-06 10:19:18 -0700

    Add Gemma to DML LLM sample (#1138)
    ## Describe your changes
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    - [ ] Is this PR including examples changes? If yes, please remember to
    update [example
    documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
    in a follow-up PR.
    
    ## (Optional) Issue link

--- examples/directml/llm/user_script.py
+++ examples/directml/llm/user_script.py
@@ -94,17 +94,16 @@ def decoder_torch_inputs(model):
     past_seq_len = 0
     sequence_length = 10
     max_seq_len = past_seq_len + sequence_length
-    head_size = config.hidden_size // config.num_heads
 
     inputs = {
         "attention_mask": torch.zeros((batch_size, max_seq_len), dtype=torch.int64),
         "past_key_values": [
             {
                 "key": torch.rand(
-                    (batch_size, config.num_key_value_heads, past_seq_len, head_size), dtype=torch.float32
+                    (batch_size, config.num_key_value_heads, past_seq_len, config.head_dim), dtype=torch.float32
                 ),
                 "value": torch.rand(
-                    (batch_size, config.num_key_value_heads, past_seq_len, head_size), dtype=torch.float32
+                    (batch_size, config.num_key_value_heads, past_seq_len, config.head_dim), dtype=torch.float32
                 ),
             }
             for _ in range(config.num_layers)
@@ -130,7 +129,6 @@ def decoder_torch_inputs(model):
 def decoder_ort_inputs(batch_size):
     sequence_length = 10
     max_seq_len = 1024
-    head_size = config.hidden_size // config.num_heads
 
     inputs = {
         "attention_mask": torch.zeros((batch_size, max_seq_len), dtype=torch.int64),
@@ -138,10 +136,10 @@ def decoder_ort_inputs(batch_size):
 
     for layer_idx in range(config.num_layers):
         inputs[f"past_key_values.{layer_idx}.key"] = torch.rand(
-            (batch_size, config.num_key_value_heads, max_seq_len, head_size), dtype=torch.float32
+            (batch_size, config.num_key_value_heads, max_seq_len, config.head_dim), dtype=torch.float32
         )
         inputs[f"past_key_values.{layer_idx}.value"] = torch.rand(
-            (batch_size, config.num_key_value_heads, max_seq_len, head_size), dtype=torch.float32
+            (batch_size, config.num_key_value_heads, max_seq_len, config.head_dim), dtype=torch.float32
         )
 
     if config.model_type == "llava":
@@ -182,7 +180,6 @@ class PileDataloader:
         self.seqlen = seqlen
         self.max_seq_len = max_seq_len
         self.batch_size = batch_size
-        self.head_size = config.hidden_size // config.num_heads
         self.dataset = load_dataset("NeelNanda/pile-10k", split=sub_folder)
         self.dataset = self.dataset.map(tokenize_function, batched=True)
         self.dataset.set_format(type="torch", columns=["input_ids", "attention_mask"])
@@ -217,10 +214,10 @@ class PileDataloader:
 
             for layer_index in range(config.num_layers):
                 initial_inputs[f"past_key_values.{layer_index}.key"] = np.zeros(
-                    (1, config.num_key_value_heads, self.max_seq_len, self.head_size), dtype=np.float16
+                    (1, config.num_key_value_heads, self.max_seq_len, config.head_dim), dtype=np.float16
                 )
                 initial_inputs[f"past_key_values.{layer_index}.value"] = np.zeros(
-                    (1, config.num_key_value_heads, self.max_seq_len, self.head_size), dtype=np.float16
+                    (1, config.num_key_value_heads, self.max_seq_len, config.head_dim), dtype=np.float16
                 )
 
             yield initial_inputs, 0
