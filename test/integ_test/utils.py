232bb75a8f0f55729c4fd16975a91ee5add58148
trajepl <jiapli@microsoft.com>
2024-05-06 16:26:38 +0800

    remove token auth

--- test/integ_test/utils.py
+++ test/integ_test/utils.py
@@ -35,13 +35,14 @@ def get_olive_workspace_config():
     }
 
 
-def download_azure_blob(container, blob, download_path):
-    try:
-        conn_str = os.environ["OLIVEWHEELS_STORAGE_CONNECTION_STRING"]
-    except KeyError as e:
-        raise Exception("Please set the environment variable OLIVEWHEELS_STORAGE_CONNECTION_STRING") from e
+def download_azure_blob(container, blob, download_path, storage_account="olivewheels"):
+    from azure.identity import ManagedIdentityCredential
+    from azure.storage.blob import BlobClient
 
-    blob = BlobClient.from_connection_string(conn_str=conn_str, container_name=container, blob_name=blob)
+    blob = BlobClient.from_blob_url(
+        f"https://{storage_account}.blob.core.windows.net/{container}/{blob}",
+        credential=ManagedIdentityCredential(client_id=os.environ.get("MANAGED_IDENTITY_CLIENT_ID")),
+    )
 
     with open(download_path, "wb") as my_blob:
         blob_data = blob.download_blob()
