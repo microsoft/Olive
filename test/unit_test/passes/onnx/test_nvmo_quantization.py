40845a3b51ef3ae9aa9ff6801e4fa633ca8145d3
Riyad Islam <mohammadshahedulislam@gmail.com>
2024-05-09 12:19:34 -0700

    NV-ModelOPT INT4 quantization (#1135)
    ## Describe your changes
    
    ## Checklist before requesting a review
    - [x] Add unit tests for this change.
    - [x] Make sure all tests can pass.
    - [x] Update documents if necessary.
    - [x] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    - [ ] Is this PR including examples changes? If yes, please remember to
    update [example
    documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
    in a follow-up PR.
    
    ## Tests
    - Unit test: `pytest
    test/unit_test/passes/onnx/test_nvmo_quantization.py`
    - Example: `python -m olive.workflows.run --config bert_nvmo_ptq.json`
    
    ## (Optional) Issue link

---
+++ test/unit_test/passes/onnx/test_nvmo_quantization.py
@@ -0,0 +1,52 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+from pathlib import Path
+from test.unit_test.utils import get_onnx_model, get_pytorch_model_dummy_input
+from typing import Any, Dict, Optional
+
+from onnxruntime.quantization.calibrate import CalibrationDataReader  # type: ignore[import]
+
+from olive.passes.olive_pass import create_pass_from_dict
+from olive.passes.onnx.nvmo_quantization import NVModelOptQuantization
+
+
+class DummyCalibrationDataReader(CalibrationDataReader):
+    def __init__(self, data_dir: str, batch_size: int = 16):
+        super().__init__()
+        self.sample_counter = 64
+
+    def get_next(self) -> Optional[Dict[Any, Any]]:
+        if self.sample_counter <= 0:
+            return None
+
+        data = get_pytorch_model_dummy_input()
+        try:
+            item = {"input": data.numpy()}
+            self.sample_counter -= 1
+            return item
+        except Exception:
+            return None
+
+
+def dummpy_dataloader_func(data_dir, batch_size, *args, **kwargs):
+    return DummyCalibrationDataReader(data_dir, batch_size=batch_size)
+
+
+def test_nvmo_quantization(tmp_path):
+    ov_model = get_onnx_model()
+    data_dir = tmp_path / "data"
+    data_dir.mkdir(exist_ok=True)
+    config = {"data_dir": data_dir, "dataloader_func": dummpy_dataloader_func}
+    output_folder = str(tmp_path / "quantized")
+
+    # create NVModelOptQuantization pass and run quantization
+    p = create_pass_from_dict(NVModelOptQuantization, config, disable_search=True)
+    quantized_model = p.run(ov_model, None, output_folder)
+
+    # assert
+    assert quantized_model.model_path.endswith(".onnx")
+    assert Path(quantized_model.model_path).exists()
+    assert Path(quantized_model.model_path).is_file()
+    assert "DequantizeLinear" in [node.op_type for node in quantized_model.load_model().graph.node]
