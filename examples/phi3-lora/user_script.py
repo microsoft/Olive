02fe07ac375e6911efa23b69d6438ff2af64dfbb
Jambay Kinley <jambaykinley@microsoft.com>
2024-05-16 18:42:16 +0000

    mb-lora

---
+++ examples/phi3-lora/user_script.py
@@ -0,0 +1,20 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+from typing import Union
+
+from datasets import load_dataset
+
+from olive.data.registry import Registry
+
+# -----------------------------------------------------------------------------
+#  QLoRA load_dataset component
+# -----------------------------------------------------------------------------
+
+
+@Registry.register_dataset()
+def load_tiny_code_dataset(data_name: str, split: str, language: str, token: Union[bool, str] = True):
+    dataset = load_dataset(data_name, split=split, token=token)
+    return dataset.filter(lambda x: x["programming_language"] == language)
