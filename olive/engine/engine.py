1299c3b83ec6d4e0a12e2ec6fc43437157c45325
Xiaoyu <xiaoyuzhang@microsoft.com>
2024-05-24 07:43:19 +0000

    fix lint

--- olive/engine/engine.py
+++ olive/engine/engine.py
@@ -907,7 +907,7 @@ class Engine:
             # download model files
             logger.info("Cloud model cache is enabled. Download final model files ...")
             cloud_cache_map = self.cloud_cache_helper.download_model_cache_map()
-            cloud_model_path = self.cloud_cache_helper.exist_in_model_cache_map(cloud_cache_map, output_model_hash)
+            cloud_model_path = self.cloud_cache_helper.exist_in_model_cache_map(output_model_hash)
             self.cloud_cache_helper.update_model_config(cloud_model_path, model_config, output_model_hash)
 
         if not should_prune:
