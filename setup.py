68978f242b3fd8955d584648693a4f4c7cf8fa13
Xiaoyu <85524621+xiaoyu-work@users.noreply.github.com>
2024-07-11 13:22:03 -0700

    remove sample codes from zipfile packaging (#1226)
    ## Describe your changes
    
    remove sample codes from zipfile packaging
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    - [ ] Is this PR including examples changes? If yes, please remember to
    update [example
    documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
    in a follow-up PR.
    
    ## (Optional) Issue link

--- setup.py
+++ setup.py
@@ -83,7 +83,6 @@ setup(
     package_data={
         "olive": ["olive_config.json"],
         "olive.auto_optimizer": ["config_template/*.yaml"],
-        "olive.engine.packaging": ["sample_code/*/*/*"],
         "olive.platform_sdk.qualcomm": ["create_python_env.sh", "create_python_env.ps1", "copy_libcdsprpc.ps1"],
         "olive.systems.docker": ["Dockerfile*"],
         "olive.systems.python_environment": ["common_requirements.txt"],
