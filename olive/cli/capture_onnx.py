fd455eacb19b44965c8de5bff702cab7096ba9fc
Devang Patel <47577486+devang-ml@users.noreply.github.com>
2024-08-26 13:22:12 -0700

    Add MB options

--- olive/cli/capture_onnx.py
+++ olive/cli/capture_onnx.py
@@ -10,15 +10,21 @@ import tempfile
 from argparse import ArgumentParser
 from copy import deepcopy
 from pathlib import Path
-from typing import ClassVar, Dict, Union
+from typing import Any, ClassVar, Dict, Union
 
 import yaml
 
 from olive.cli.base import BaseOliveCLICommand
 from olive.common.utils import hardlink_copy_dir, hash_dict, set_nested_dict_value, set_tempdir
+from olive.common.utils import IntEnumBase, StrEnumBase
 
 logger = logging.getLogger(__name__)
 
+class ModelBuilder_AccuracyLevel(IntEnumBase):
+    fp32 = 1
+    fp16 = 2
+    bf16 = 3
+    int8 = 4
 
 class CaptureOnnxGraphCommand(BaseOliveCLICommand):
     allow_unknown_args: ClassVar[bool] = True
@@ -33,54 +39,11 @@ class CaptureOnnxGraphCommand(BaseOliveCLICommand):
         )
 
         sub_parser.add_argument(
-            "--use_dynamo_exporter",
-            type=bool,
-            default=False,
-            help="Whether to use dynamo_export API to export ONNX model.",
-        )
-
-        sub_parser.add_argument(
-            "--use_model_builder",
-            action="store_true",
-            help="Whether to use Model Builder to capture ONNX model.",
-        )
-
-        sub_parser.add_argument(
-            "--past_key_value_name",
-            type=str,
-            default="past_key_values",
-            help=(
-                "The arguments name to point to past key values. For model loaded from huggingface, "
-                "it is 'past_key_values'. Basically, it is used only when `use_dynamo_exporter` is True."
-            )
-        )
-
-        sub_parser.add_argument(
-            "--device",
-            type=str,
-            help=(
-                "The device to use to trace the model, e.g., 'cuda' or 'cpu'. If not specified, will"
-                " use 'cpu' for PyTorch model and 'cuda' for the distributed model."
-            ),
-        )
-
-        sub_parser.add_argument(
-            "--torch_dtype",
-            type=str,
-            help=(
-                "The dtype to cast the model to before capturing the ONNX graph, e.g., 'float32' or 'float16'."
-                " If not specified will use the model as is."
-            ),
-        )
-
-        sub_parser.add_argument(
             "--use_ort_genai",
             type=bool,
             default=False,
             help="Use OnnxRuntie generate() API to run the model"
         )
-
-        # directory options
         sub_parser.add_argument(
             "-o", "--output_path", 
             type=str, 
@@ -89,6 +52,92 @@ class CaptureOnnxGraphCommand(BaseOliveCLICommand):
             help="Output path")
         sub_parser.add_argument(
             "--tempdir", default=None, type=str, help="Root directory for tempfile directories and files"
+        )
+
+        # PyTorch Exporter options
+        pte_group = sub_parser.add_argument_group("PyTorch Exporter options")
+        pte_group.add_argument(
+            "--use_dynamo_exporter",
+            type=bool,
+            default=False,
+            help="Whether to use dynamo_export API to export ONNX model.",
+        )
+        pte_group.add_argument(
+            "--past_key_value_name",
+            type=str,
+            default="past_key_values",
+            help=(
+                "The arguments name to point to past key values. For model loaded from huggingface, "
+                "it is 'past_key_values'. Basically, it is used only when `use_dynamo_exporter` is True."
+            )
+        )
+        pte_group.add_argument(
+            "--device",
+            type=str,
+            help=(
+                "The device to use to trace the model, e.g., 'cuda' or 'cpu'. If not specified, will"
+                " use 'cpu' for PyTorch model and 'cuda' for the distributed model."
+            ),
+        )
+        pte_group.add_argument(
+            "--torch_dtype",
+            type=str,
+            help=(
+                "The dtype to cast the model to before capturing the ONNX graph, e.g., 'float32' or 'float16'."
+                " If not specified will use the model as is."
+            ),
+        )
+
+        # Model Builder options
+        mb_group = sub_parser.add_argument_group("Model Builder options")
+        mb_group.add_argument(
+            "--use_model_builder",
+            action="store_true",
+            help="Whether to use Model Builder to capture ONNX model.",
+        )
+        mb_group.add_argument(
+            "--precision",
+            type=str,
+            default="fp16",
+            choices=["fp16", "fp32", "int4"],
+            help="The precision of the ONNX model. This is used by Model Builder",
+        )
+        mb_group.add_argument(
+            "--int4_block_size",
+            type=int,
+            required=False,
+            help="Specify the block_size for int4 quantization. Acceptable values: 16/32/64/128/256.",
+        )
+        mb_group.add_argument(
+            "--int4_accuracy_level",
+            type=ModelBuilder_AccuracyLevel,
+            required=False,
+            help="Specify the minimum accuracy level for activation of MatMul in int4 quantization.",
+        )
+        mb_group.add_argument(
+            "--exclude_embeds",
+            type=bool,
+            default=False,
+            required=False,
+            help="Remove embedding layer from your ONNX model.",
+        )
+        mb_group.add_argument(
+            "--exclude_lm_head",
+            type=bool,
+            default=False,
+            required=False,
+            help="Remove language modeling head from your ONNX model.",
+        )
+        mb_group.add_argument(
+            "--enable_cuda_graph",
+            type=bool,
+            default=None,  # Explicitly setting to None to differentiate between user intent and default.
+            required=False,
+            help=(
+                "The model can use CUDA graph capture for CUDA execution provider. "
+                "If enabled, all nodes being placed on the CUDA EP is the prerequisite "
+                "for the CUDA graph to be used correctly."
+            ),
         )
 
         # model options
@@ -103,13 +152,13 @@ class CaptureOnnxGraphCommand(BaseOliveCLICommand):
                 " path as 'registry_name:model_name:version'."
             ),
         )
-
         model_group.add_argument(
             "-t",
             "--task",
             type=str,
             help="Task for which the model is used."
         )
+
         sub_parser.set_defaults(func=CaptureOnnxGraphCommand)
 
     def run(self):
@@ -149,8 +198,19 @@ class CaptureOnnxGraphCommand(BaseOliveCLICommand):
 
         if self.args.use_model_builder:
             del config["passes"]["c"]
+            config["passes"]["m"]["precision"] = self.args.precision
+            if self.args.int4_block_size is not None:
+                config["passes"]["m"]["int4_block_size"] = self.args.int4_block_size
+            if self.args.int4_accuracy_level is not None:
+            config["passes"]["m"]["int4_accuracy_level"] = self.args.int4_accuracy_level
+            config["passes"]["m"]["exclude_embeds"] = self.args.exclude_embeds
+            config["passes"]["m"]["exclude_lm_head"] = self.args.exclude_lm_head
+            config["passes"]["m"]["enable_cuda_graph"] = self.args.enable_cuda_graph
         else:
             del config["passes"]["m"]
+            config["passes"]["c"]["past_key_value_name"] = self.args.past_key_value_name
+            config["passes"]["c"]["device"] = self.args.device
+            config["passes"]["c"]["torch_dtype"] = self.args.torch_dtype
             config["passes"]["c"]["save_metadata_for_token_generation"] = self.args.use_ort_genai
 
         print(f'.... {config}')
