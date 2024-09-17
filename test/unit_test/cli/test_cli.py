1b27d809822598848829f90b2b173e8ec7af7c15
Jambay Kinley <jambaykinley@microsoft.com>
2024-09-17 18:33:28 +0000

    fix cli tests

--- test/unit_test/cli/test_cli.py
+++ test/unit_test/cli/test_cli.py
@@ -101,7 +101,7 @@ def test_configure_qualcomm_sdk_command(mock_configure):
     mock_configure.assert_called_once_with("3.6", "snpe")
 
 
-@patch("olive.workflows.run")
+@patch("olive.workflows.run", return_value={"dummy0": Footprint(nodes={"dummy1": "dummy2"})})
 @patch("olive.cli.finetune.tempfile.TemporaryDirectory")
 def test_finetune_command(mock_tempdir, mock_run, tmp_path):
     # some directories
@@ -112,12 +112,11 @@ def test_finetune_command(mock_tempdir, mock_run, tmp_path):
 
     # setup
     mock_tempdir.return_value = tmpdir.resolve()
-    mock_run.return_value = {"output_dir": Footprint(nodes={"dummy_output": "dummy_output"})}
-    workflow_output_dir = tmpdir / "f-c-o-e" / "gpu-cuda_model"
+    workflow_output_dir = tmpdir / "output_model"
     workflow_output_dir.mkdir(parents=True)
-    dummy_output = workflow_output_dir / "dummy_output"
+    dummy_output = workflow_output_dir / "model_config.json"
     with open(dummy_output, "w") as f:
-        f.write("dummy_output")
+        json.dump({"dummy": "output"}, f)
 
     # setup
     command_args = [
@@ -140,11 +139,10 @@ def test_finetune_command(mock_tempdir, mock_run, tmp_path):
     assert {el.name for el in output_dir.iterdir()} == {dummy_output.name}
 
 
-@patch("olive.cli.perf_tuning.olive_run")
 @patch("olive.cli.perf_tuning.tempfile.TemporaryDirectory")
-@patch("olive.common.ort_inference.get_ort_inference_session")
-@pytest.mark.parametrize("data_config_path", ["", "dummy_data_config_path.json"])
-def test_perf_tuning_command(mock_ort_infer_sess, mock_tempdir, mock_run, data_config_path, tmp_path):
+def test_perf_tuning_command(mock_tempdir, tmp_path):
+    from test.unit_test.utils import ONNX_MODEL_PATH
+
     # some directories
     tmpdir = tmp_path / "tmpdir"
     tmpdir.mkdir()
@@ -152,53 +150,57 @@ def test_perf_tuning_command(mock_ort_infer_sess, mock_tempdir, mock_run, data_c
     output_dir = tmp_path / "output_dir"
 
     # setup
-    data_config = {
-        "name": "dummy_data",
-        "type": "TransformersTokenDummyDataContainer",
-        "load_dataset_config": {"model_name": "microsoft/phi-2"},
-    }
-    if data_config_path:
-        data_config_path = str(tmpdir / data_config_path)
-        with open(data_config_path, "w") as f:
-            json.dump(data_config, f)
     mock_tempdir.return_value = tmpdir.resolve()
-    workflow_output_dir = tmpdir / "perf_tuning" / "gpu-cuda-model"
-    workflow_output_dir.mkdir(parents=True)
-    dummy_output = workflow_output_dir / "dummy_output"
-    with open(dummy_output, "w") as f:
-        f.write("dummy_output")
+    data_config = {
+        "name": "test_data_config_for_tuning",
+        "type": "DummyDataContainer",
+        "load_dataset_config": {"input_shapes": [(1, 1)], "input_names": ["input"]},
+    }
+    data_config_path = str(tmpdir / "data_config.json")
+    with open(data_config_path, "w") as f:
+        json.dump(data_config, f)
 
     # setup
     command_args = [
         "tune-session-params",
         "--model",
-        "dummy_model",
+        str(ONNX_MODEL_PATH),
         "--data_config_path",
         data_config_path,
-        "--hf_model_name",
-        "Intel/bert-base-uncased-mrpc",
         "--output_path",
         str(output_dir),
+        "--providers_list",
+        "CPUExecutionProvider",
     ]
 
     # execute
     cli_main(command_args)
 
-    config = mock_run.call_args[0][0]
-    assert config["input_model"]["model_path"] == "dummy_model"
-    assert config["data_configs"][0].name == config["passes"]["perf_tuning"]["data_config"]
+    with open(output_dir / "cpu-cpu.json") as f:
+        infer_settings = json.load(f)
+        assert infer_settings["execution_provider"] == ["CPUExecutionProvider"]
+        assert infer_settings.keys() >= {"provider_options", "session_options"}
 
 
-@patch("olive.workflows.run")
+@patch("olive.workflows.run", return_value={"dummy0": Footprint(nodes={"dummy1": "dummy2"})})
 @patch("olive.cli.capture_onnx.tempfile.TemporaryDirectory")
 @pytest.mark.parametrize("use_model_builder", [True, False])
 def test_capture_onnx_command(mock_tempdir, mock_run, use_model_builder, tmp_path):
-    # setup
-    mock_tempdir.return_value = tmp_path.resolve()
+    # some directories
+    tmpdir = tmp_path / "tmpdir"
+    tmpdir.mkdir()
+
     output_dir = tmp_path / "output_dir"
-    model_id = "microsoft/phi-2"
 
     # setup
+    mock_tempdir.return_value = tmpdir.resolve()
+    workflow_output_dir = tmpdir / "output_model"
+    workflow_output_dir.mkdir(parents=True)
+    dummy_output = workflow_output_dir / "model_config.json"
+    with open(dummy_output, "w") as f:
+        json.dump({"config": {"inference_settings": {"dummy-key": "dummy-value"}}}, f)
+
+    model_id = "dummy-model-id"
     command_args = [
         "capture-onnx-graph",
         "-m",
@@ -216,11 +218,13 @@ def test_capture_onnx_command(mock_tempdir, mock_run, use_model_builder, tmp_pat
     config = mock_run.call_args[0][0]
     assert config["input_model"]["model_path"] == model_id
     assert "m" in config["passes"] if use_model_builder else "c" in config["passes"]
+    assert {el.name for el in output_dir.iterdir()} == {dummy_output.name}
 
 
 @pytest.mark.parametrize("test_set", [(None, "successfully"), (MagicMock(name="blob1"), "failed")])
 @patch("azure.storage.blob.ContainerClient")
-def test_cloud_cache_command(mock_container_client, test_set):
+@patch("olive.cli.cloud_cache.get_credentials", return_value="dummy-credentials")
+def test_cloud_cache_command(_, mock_container_client, test_set):
     # setup
     command_args = [
         "cloud-cache",
@@ -241,7 +245,9 @@ def test_cloud_cache_command(mock_container_client, test_set):
 
     # assert
     assert (test_set[1] in message for message in log.output), "Expected log message not found."
-    mock_container_client.assert_called_once()
+    mock_container_client.assert_called_once_with(
+        account_url="https://account.blob.core.windows.net", container_name="container", credential="dummy-credentials"
+    )
     mock_container_client().delete_blob.assert_called_once()
 
 
