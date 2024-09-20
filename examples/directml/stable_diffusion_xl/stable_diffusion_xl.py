fac39efedd990e71a80cc90fc55b7770dfeaf63d
Vishal Agarwal <vishala@nvidia.com>
2024-09-20 15:17:23 +0530

    fix sdxl optimization

--- examples/directml/stable_diffusion_xl/stable_diffusion_xl.py
+++ examples/directml/stable_diffusion_xl/stable_diffusion_xl.py
@@ -366,7 +366,7 @@ def optimize(
 
         olive_run(olive_config)
 
-        footprints_file_path = Path(__file__).resolve().parent / "footprints" / "footprints.json"
+        footprints_file_path = Path(__file__).resolve().parent / "footprints" / submodel_name / "footprints.json"
         with footprints_file_path.open("r") as footprint_file:
             footprints = json.load(footprint_file)
 
