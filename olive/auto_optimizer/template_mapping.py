efbdcf6fabea9b790e43841b439764fce56f7525
Xiaoyu Zhang <xiaoyuzhang@microsoft.com>
2024-10-01 12:40:15 -0700

    Removed unused template

--- olive/auto_optimizer/template_mapping.py
+++ olive/auto_optimizer/template_mapping.py
@@ -15,9 +15,6 @@ TEMPLATE_CONFIG_PATH = Path(__file__).resolve().parent / "config_template"
 PASS_CAPABILITY = "pass_capability.yaml"
 OPT_LEVEL_PASSES = "opt_level_passes.yaml"
 
-WORKFLOW_TEMPLATE_PATH = Path(__file__).resolve().parent / "workflow_template"
-SESSION_PARAMS_TUNING_TEMPLATE = WORKFLOW_TEMPLATE_PATH / "ort_session_params_tuning.yaml"
-
 
 def get_pass_capability():
     with (TEMPLATE_CONFIG_PATH / PASS_CAPABILITY).open() as f:
