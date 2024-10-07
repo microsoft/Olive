854f4e599e8c12022540b1df21a0ab7c8b591d0a
Jambay Kinley <jambaykinley@microsoft.com>
2024-10-07 14:55:58 -0700

    CI: Fix test dependencies (#1409)
    ## Describe your changes
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    - [ ] Is this PR including examples changes? If yes, please remember to
    update [example
    documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
    in a follow-up PR.
    
    ## (Optional) Issue link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,6 @@ suppress_warnings = ["myst.xref_missing"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ["_static"]
 html_css_files = [
