microsoft/Olive

commit 5925e72dfe42ef2f8a2f0f4a46eed3b7182e2fb2

Redo SessionParamsTuning pass (#1401)

examples/phi3/phi3.py
@@ -82,6 +82,11 @@ def get_args(raw_args):
         action="store_true",
         help="Run AWQ on the base model or the finetuned model",
     )
+    quant_group.add_argument(
+        "--tune-session-params",
+        action="store_true",
+        help="Tune onnx session params",
+    )
 
     parser.add_argument(
         "--precision",
@@ -243,6 +248,11 @@ def generate_config(args):
             print("AWQ only supports int4 precision. Changing precision to int4")
             args.precision = "int4"
     passes_to_use.append("builder")
+
+    if args.tune_session_params:
+        passes_to_use.append("tune_session_params")
+    else:
+        del template_json["search_strategy"]
 
     target = str(args.target)
     if target == "web":

examples/test/local/test_bert_cuda_gpu.py
@@ -19,12 +19,11 @@ def setup():
 @pytest.mark.parametrize("execution_order", ["joint", "pass-by-pass"])
 @pytest.mark.parametrize("system", ["local_system"])
 @pytest.mark.parametrize("olive_json", ["bert_cuda_gpu.json"])
-@pytest.mark.parametrize("enable_cuda_graph", [True, False])
-def test_bert(search_algorithm, execution_order, system, olive_json, enable_cuda_graph):
+def test_bert(search_algorithm, execution_order, system, olive_json):
     from olive.workflows import run as olive_run
 
     olive_config = patch_config(olive_json, search_algorithm, execution_order, system, is_gpu=True)
-    olive_config["passes"]["session_params_tuning"]["enable_cuda_graph"] = enable_cuda_graph
+    olive_config["passes"]["session_params_tuning"]["enable_cuda_graph"] = False
 
     footprint = olive_run(olive_config, tempdir=os.environ.get("OLIVE_TEMPDIR", None))
     check_output(footprint)

olive/engine/packaging/packaging_generator.py
@@ -332,7 +332,7 @@ def _get_best_candidate_node(
 
 def _is_generative_model(config: Dict[str, Any]) -> bool:
     model_attributes = config.get("model_attributes") or {}
-    return model_attributes.get("is_generative", False)
+    return model_attributes.get("generative", False)
 
 
 def _package_candidate_models(

olive/evaluator/olive_evaluator.py
@@ -91,7 +91,7 @@ class OliveEvaluator(ABC):
             # we cannot generate dummy data for the onnx model if this model has dynamic input shapes
             # TODO(trajep): try to get static input shapes from onnx model.
             # If so, we can move the dataloader for latency measurement.
-            logger.debug(
+            logger.error(
                 "Model input shapes are not static. Cannot use inferred input shapes for creating dummy data. This will"
                 " cause an error when creating dummy data for tuning."
             )

olive/passes/onnx/model_builder.py
@@ -222,8 +222,12 @@ class ModelBuilder(Pass):
         with open(genai_config_filepath, "w") as ostrm:
             json.dump(genai_config, ostrm, indent=4)
 
+        # Save HfModel config
+        if isinstance(model, HfModelHandler):
+            model.get_hf_model_config().save(str(output_model_filepath.parent))
+
         # add additional files generated by model builder to model_attributes
-        model_attributes["is_generative"] = True
+        model_attributes["generative"] = True
         additional_files = model_attributes.get("additional_files") or []
         if metadata_only:
             # add genai_config.json to additional_files since the model_builder creates copy of the other files
@@ -232,6 +236,9 @@ class ModelBuilder(Pass):
                 *additional_files,
                 str(output_model_filepath.parent / "genai_config.json"),
             ]
+
+            if isinstance(model, HfModelHandler):
+                model_attributes["additional_files"].append(str(output_model_filepath.parent / "config.json"))
         else:
             model_attributes["additional_files"] = sorted(
                 set(additional_files)

test/unit_test/passes/onnx/test_session_params_tuning.py
@@ -3,26 +3,18 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 import logging
-import math
-import re
 from test.unit_test.utils import get_onnx_model
-from unittest.mock import MagicMock, PropertyMock, patch
+from unittest.mock import PropertyMock, patch
 
-import psutil
 import pytest
 
 from olive.common.config_utils import validate_config
 from olive.data.config import DataComponentConfig, DataConfig
 from olive.evaluator.metric_result import flatten_metric_result
 from olive.evaluator.olive_evaluator import OliveEvaluator, OnnxEvaluator
-from olive.hardware.accelerator import DEFAULT_CPU_ACCELERATOR, DEFAULT_GPU_CUDA_ACCELERATOR, AcceleratorSpec, Device
+from olive.hardware.accelerator import DEFAULT_GPU_CUDA_ACCELERATOR
 from olive.passes.olive_pass import create_pass_from_dict
-from olive.passes.onnx.session_params_tuning import (
-    PERFTUNING_BASELINE,
-    OrtSessionParamsTuning,
-    PerfTuningRunner,
-    generate_test_name,
-)
+from olive.passes.onnx.session_params_tuning import OrtSessionParamsTuning
 
 
 def _get_tuning_data_config(input_shapes, input_names=None):
@@ -63,7 +55,7 @@ def test_ort_session_params_tuning_pass(config, tmp_path):
     [
         {},
         {"data_config": _get_tuning_data_config([(1, 1)], ["input"])},
-        {"providers_list": ["CPUExecutionProvider", "CUDAExecutionProvider"], "device": "gpu"},
+        {"providers_list": "CUDAExecutionProvider", "device": "gpu"},
     ],
 )
 def test_ort_session_params_tuning_with_customized_configs(mock_run, config):
@@ -76,9 +68,9 @@ def test_ort_session_params_tuning_with_customized_configs(mock_run, config):
 
     # assert
     if "providers_list" not in config:
-        assert mock_run.call_args.args[1]["providers_list"] == [
-            "CPUExecutionProvider"
-        ], "providers_list is not set correctly as ['CPUExecutionProvider'] by default when user does not specify it"
+        assert (
+            mock_run.call_args.args[1]["providers_list"] == "CPUExecutionProvider"
+        ), "providers_list is not set correctly as ['CPUExecutionProvider'] by default when user does not specify it"
     if "device" not in config:
         assert (
             mock_run.call_args.args[1]["device"] == "cpu"
@@ -87,11 +79,28 @@ def test_ort_session_params_tuning_with_customized_configs(mock_run, config):
         assert mock_run.call_args.args[1][k] == v, f"{k} is not set correctly as {v}"
 
 
-@pytest.mark.parametrize("return_baseline", [True, False])
+@pytest.mark.parametrize(
+    ("execution_provider", "provider_options"),
+    [
+        ("TensorrtExecutionProvider", {"trt_fp16_enable": True}),
+        (
+            "CUDAExecutionProvider",
+            {
+                "device_id": 0,
+                "arena_extend_strategy": "kNextPowerOfTwo",
+                "gpu_mem_limit": 2 * 1024 * 1024 * 1024,
+                "cudnn_conv_algo_search": "EXHAUSTIVE",
+                "do_copy_in_default_stream": True,
+                "enable_cuda_graph": False,
+            },
+        ),
+        ("CPUExecutionProvider", {}),
+    ],
+)
 @patch.object(OnnxEvaluator, "evaluate")
 @patch("onnxruntime.get_available_providers")
 def test_session_params_tuning_with_provider_options(
-    get_available_providers_mock, evaluate_mock, caplog, return_baseline
+    get_available_providers_mock, evaluate_mock, execution_provider, provider_options
 ):
     logger = logging.getLogger("olive")
     logger.propagate = True
@@ -108,44 +117,21 @@ def test_session_params_tuning_with_provider_options(
         assert execution_providers is None
         ep = metrics[0].user_config.inference_settings["onnx"]["execution_provider"]
         if len(ep) == 1:
-            # for single perf tuning case
-            if return_baseline:
-                latency = 0.6
+            if ep[0] == "CUDAExecutionProvider":
+                latency = 0.4
+            elif ep[0] == "TensorrtExecutionProvider":
+                latency = 0.7
             else:
-                if ep[0] == "CUDAExecutionProvider":
-                    latency = 0.4
-                elif ep[0] == "TensorrtExecutionProvider":
-                    latency = 0.7
-                else:
-                    latency = 0.5
+                latency = 0.5
 
         latency_metric = OliveEvaluator.compute_latency(metrics[0], [latency])
         metrics_res[metrics[0].name] = latency_metric
         return flatten_metric_result(metrics_res)
 
     evaluate_mock.side_effect = mock_evaluate_method
-    execution_providers = [
-        (
-            "TensorrtExecutionProvider",
-            {
-                "trt_fp16_enable": True,
-            },
-        ),
-        [
-            "CUDAExecutionProvider",
-            {
-                "device_id": 0,
-                "arena_extend_strategy": "kNextPowerOfTwo",
-                "gpu_mem_limit": 2 * 1024 * 1024 * 1024,
-                "cudnn_conv_algo_search": "EXHAUSTIVE",
-                "do_copy_in_default_stream": True,
-                "enable_cuda_graph": False,
-            },
-        ],
-        "CPUExecutionProvider",
-    ]
     config = {
-        "providers_list": execution_providers,
+        "providers_list": execution_provider,
+        "provider_options_list": provider_options,
         "device": "gpu",
         "enable_cuda_graph": True,
     }
@@ -157,82 +143,14 @@ def test_session_params_tuning_with_provider_options(
     assert "execution_provider" in result.inference_settings
     acutal_eps = result.inference_settings["execution_provider"]
     assert "io_bind" in result.inference_settings
-    if return_baseline:
-        assert acutal_eps == ["TensorrtExecutionProvider", "CUDAExecutionProvider", "CPUExecutionProvider"]
-        assert "enable_cuda_graph" not in result.inference_settings["provider_options"][0]
-        assert re.search(f"Best result:.*{PERFTUNING_BASELINE}", caplog.text)
-    else:
-        assert len(acutal_eps) == 1
-        assert acutal_eps[0] == "CUDAExecutionProvider"
+    assert acutal_eps == [execution_provider]
+    if execution_provider == "CUDAExecutionProvider":
         assert result.inference_settings["provider_options"][0][
             "enable_cuda_graph"
-        ], "enable_cuda_graph is should be overridden to True"
+        ], "enable_cuda_graph should be overridden to True"
         assert result.inference_settings["provider_options"][0]["arena_extend_strategy"] == "kNextPowerOfTwo"
-
-
-@pytest.mark.parametrize("force_evaluate", [True, False])
-@patch.object(OnnxEvaluator, "evaluate")
-@patch("onnxruntime.get_available_providers")
-def test_session_params_tuning_with_force_evaluate(get_available_providers_mock, evaluate_mock, caplog, force_evaluate):
-    logger = logging.getLogger("olive")
-    logger.propagate = True
-
-    get_available_providers_mock.return_value = [
-        "TensorrtExecutionProvider",
-        "CUDAExecutionProvider",
-        "CPUExecutionProvider",
-    ]
-
-    def mock_evaluate_method(model, metrics, device, execution_providers):
-        metrics_res = {}
-        latency = 0.5
-        assert execution_providers is None
-        ep = metrics[0].user_config.inference_settings["onnx"]["execution_provider"]
-        if len(ep) == 1:
-            if ep[0] == "CPUExecutionProvider":
-                latency = 0.5
-            elif ep[0] == "CUDAExecutionProvider":
-                latency = 0.1
-        else:
-            assert len(ep) == 2
-            latency = 0.6
-
-        latency_metric = OliveEvaluator.compute_latency(metrics[0], [latency])
-        metrics_res[metrics[0].name] = latency_metric
-        return flatten_metric_result(metrics_res)
-
-    evaluate_mock.side_effect = mock_evaluate_method
-    execution_providers = [
-        "CUDAExecutionProvider",
-        "CPUExecutionProvider",
-    ]
-    config = {
-        "providers_list": execution_providers,
-        "device": "gpu",
-        "force_evaluate_other_eps": force_evaluate,
-    }
-    input_model = get_onnx_model()
-    p = create_pass_from_dict(
-        OrtSessionParamsTuning, config, disable_search=True, accelerator_spec=DEFAULT_CPU_ACCELERATOR
-    )
-    result = p.run(input_model, None)
-    assert "io_bind" in result.inference_settings
-    if force_evaluate:
-        assert "execution_provider" in result.inference_settings
-        acutal_eps = result.inference_settings["execution_provider"]
-        assert len(acutal_eps) == 1
-        assert acutal_eps[0] == "CUDAExecutionProvider"
-        assert re.search("Best result:.*cuda", caplog.text)
     else:
-        assert "execution_provider" in result.inference_settings
-        acutal_eps = result.inference_settings["execution_provider"]
-        assert len(acutal_eps) == 1
-        assert acutal_eps[0] == "CPUExecutionProvider"
-        assert re.search("Best result: .*cpu", caplog.text)
-        assert (
-            "Ignore perf tuning for EP CUDAExecutionProvider since current pass EP is CPUExecutionProvider"
-            in caplog.text
-        )
+        assert "enable_cuda_graph" not in result.inference_settings["provider_options"][0]
 
 
 @patch("olive.model.ONNXModelHandler.io_config", new_callable=PropertyMock)
@@ -254,124 +172,3 @@ def test_ort_session_params_tuning_pass_with_dynamic_shapes(mock_get_io_config, 
         # execute
         p.run(input_model, output_folder)
     assert "ones() received an invalid combination of arguments" in str(e.value)
-
-
-@patch.object(PerfTuningRunner, "threads_num_binary_search")
-def test_ort_session_params_tuning_pass_with_import_error(threads_num_binary_search_mock, tmp_path):
-    threads_num_binary_search_mock.side_effect = ModuleNotFoundError("test")
-
-    input_model = get_onnx_model()
-    p = create_pass_from_dict(OrtSessionParamsTuning, {}, disable_search=True)
-    output_folder = str(tmp_path / "onnx")
-
-    with pytest.raises(ModuleNotFoundError) as e:
-        # execute
-        p.run(input_model, output_folder)
-
-    assert "test" in str(e.value)
-
-
-def test_generate_test_name():
-    test_params = {
-        "execution_provider": ["CPUExecutionProvider"],
-        "provider_options": [{}],
-        "session_options": {
-            "execution_mode": 1,
-            "extra_session_config": None,
-            "inter_op_num_threads": 1,
-            "intra_op_num_threads": 8,
-        },
-    }
-
-    name = generate_test_name(test_params, True)
-    assert (
-        name == "cpu-{'execution_mode': 1, "
-        "'extra_session_config': None, 'inter_op_num_threads': 1, 'intra_op_num_threads': 8}"
-        "-{'io_bind': True}"
-    )
-
-    test_params = {
-        "execution_provider": ["TensorrtExecutionProvider"],
-        "provider_options": [{"trt_fp16_enable": True}],
-        "session_options": {
-            "execution_mode": 1,
-            "extra_session_config": {
-                "session.intra_op_thread_affinities": "0;1;2;3;4;5;6;7",
-            },
-            "inter_op_num_threads": 1,
-            "intra_op_num_threads": 8,
-            "graph_optimization_level": 99,
-        },
-    }
-    name = generate_test_name(test_params, False)
-    assert (
-        name == "('tensorrt', {'trt_fp16_enable': True})-"
-        "{'execution_mode': 1, "
-        "'extra_session_config': {'session.intra_op_thread_affinities': '0;1;2;3;4;5;6;7'}, "
-        "'inter_op_num_threads': 1, 'intra_op_num_threads': 8, 'graph_optimization_level': 99}"
-    )
-
-
-@patch("onnxruntime.InferenceSession")
-@patch("onnxruntime.get_available_providers")
-def test_rocm_tuning_enable(get_available_providers_mock, inference_session_mock, tmp_path):
-    tuning_result = [
-        {
-            "ep": "ROCMExecutionProvider",
-            "results": {
-                "onnxruntime::rocm::tunable::blas::internal::GemmTunableOp<__half, ck::tensor_layout::gemm::RowMajor, ck::tensor_layout::gemm::RowMajor>": {  # noqa: E501
-                    "NN_992_4096_4096": 300,
-                    "NN_992_4096_11008": 664,
-                    "NN_984_4096_4096": 1295,
-                },
-                "onnxruntime::contrib::rocm::SkipLayerNormTunableOp<__half, float, __half, true>": {
-                    "4096_4620288": 20,
-                    "4096_13697024": 39,
-                    "4096_16744448": 39,
-                },
-            },
-            "validators": {
-                "ORT_VERSION": "1.17.0",
-                "ORT_GIT_COMMIT": "",
-                "ORT_BUILD_CONFIG": "USE_CK=1|USE_ROCBLAS_EXTENSION_API=1|USE_HIPBLASLT=1|",
-                "HIP_VERSION": "50731921",
-                "ROCBLAS_VERSION": "3.1.0.b80e4220-dirty",
-                "DEVICE_MODEL": "AMD Instinct MI250X/MI250",
-            },
-        }
-    ]
-
-    mock = MagicMock()
-    mock.get_providers.return_value = ["MIGraphXExecutionProvider", "ROCMExecutionProvider"]
-    mock.get_tuning_results.return_value = tuning_result
-    inference_session_mock.return_value = mock
-
-    get_available_providers_mock.return_value = [
-        "MIGraphXExecutionProvider",
-        "ROCMExecutionProvider",
-        "CPUExecutionProvider",
-    ]
-
-    config = {
-        "providers_list": ["MIGraphXExecutionProvider", "ROCMExecutionProvider"],
-        "device": "gpu",
-    }
-
-    # setup
-    input_model = get_onnx_model()
-    p = create_pass_from_dict(
-        OrtSessionParamsTuning,
-        config,
-        disable_search=True,
-        accelerator_spec=AcceleratorSpec(accelerator_type=Device.GPU, execution_provider="ROCMExecutionProvider"),
-    )
-    output_folder = str(tmp_path / "onnx")
-
-    # execute
-    result = p.run(input_model, output_folder)
-    tuning_result_ret = result.inference_settings["tuning_op_result"]
-    assert tuning_result_ret == tuning_result
-    set_tuning_result_binary_search_count_per_iteration = int(math.log2(psutil.cpu_count(logical=False))) + 1
-    set_tuning_result_count = 3 * set_tuning_result_binary_search_count_per_iteration
-    assert mock.set_tuning_results.call_count >= set_tuning_result_count
-    assert mock.get_tuning_results.call_count == mock.set_tuning_results.call_count + 1

commit 25ed55eaa6877f8a0924e5f52321ae1774ea13a8

Phi3: Fix example (#1452)
## Describe your changes
Fix example that was broken by #1401.

## Checklist before requesting a review
- [ ] Add unit tests for this change.
- [ ] Make sure all tests can pass.
- [ ] Update documents if necessary.
- [ ] Lint and apply fixes to your code by running `lintrunner -a`
- [ ] Is this a user-facing change? If yes, give a description of this
change to be included in the release notes.
- [ ] Is this PR including examples changes? If yes, please remember to
update [example
documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
in a follow-up PR.

## (Optional) Issue link
Fixes #1447

.azure_pipelines/olive-ci.yaml
@@ -18,6 +18,7 @@ trigger:
     - examples/mistral/*
     - examples/open_llama/*
     - examples/phi2/*
+    - examples/phi3/*
 pr:
   branches:
     include:
@@ -42,6 +43,7 @@ pr:
     - examples/mistral/*
     - examples/open_llama/*
     - examples/phi2/*
+    - examples/phi3/*
 
 variables:
   runCodesignValidationInjection: false

examples/phi3/phi3.py
@@ -251,8 +251,10 @@ def generate_config(args):
 
     if args.tune_session_params:
         passes_to_use.append("tune_session_params")
+        template_json["search_strategy"] = {"execution_order": "joint", "search_algorithm": "exhaustive"}
+        template_json["evaluator"] = "common_evaluator"
     else:
-        del template_json["search_strategy"]
+        del template_json["evaluators"]
 
     target = str(args.target)
     if target == "web":