b7405973cb12822def910582a8bc0351073fd670
Jambay Kinley <jambaykinley@microsoft.com>
2024-11-11 13:45:36 -0800

    OnnxModelOptimizer: Rename to OnnxPeepholeOptimizer, handle failed shape inference (#1477)

--- olive/cli/auto_opt.py
+++ olive/cli/auto_opt.py
@@ -206,8 +206,10 @@ class AutoOptCommand(BaseOliveCLICommand):
         elif self.args.provider == "CUDAExecutionProvider":
             self.args.device = "gpu"
 
+        # _get_passes_config requires input_model to be set
+        config["input_model"] = get_input_model_config(self.args)
+
         to_replace = [
-            ("input_model", get_input_model_config(self.args)),
             ("output_dir", tempdir),
             ("log_severity_level", self.args.log_level),
         ]
@@ -341,7 +343,7 @@ class AutoOptCommand(BaseOliveCLICommand):
         else:
             # qnn ep might not supported optimized model
             # will re-enable it if needed in the future
-            passes_to_remove.update(["transformer_optimizer", "optimizer"])
+            passes_to_remove.update(["transformer_optimizer", "peephole_optimizer"])
 
         if self.args.provider not in {"JsExecutionProvider", "WebGpuExecutionProvider"}:
             # JS EP doesn't support fp16 io
@@ -350,7 +352,7 @@ class AutoOptCommand(BaseOliveCLICommand):
         if self.args.use_model_builder:
             # Don't run optimizers when using model builder
             passes_to_remove.add("transformer_optimizer")
-            passes_to_remove.add("optimizer")
+            passes_to_remove.add("peephole_optimizer")
             # model already comes in int4
             passes_to_remove.add("matmul4")
 
@@ -448,7 +450,7 @@ TEMPLATE = {
                 "transformer_optimizer",
                 {"type": "OrtTransformersOptimization", "opt_level": 0, "float16": False, "keep_io_types": False},
             ),
-            ("optimizer", {"type": "OnnxModelOptimizer"}),
+            ("peephole_optimizer", {"type": "OnnxPeepholeOptimizer"}),
             # change io types to fp32
             ("fp16_to_fp32", {"type": "OnnxIOFloat16ToFloat32"}),
             # qnn preparation passes
@@ -476,7 +478,7 @@ PRECISION_MAPPING = {
     "conversion": {},
     "model_builder": {},
     "transformer_optimizer": {},
-    "optimizer": {},
+    "peephole_optimizer": {},
     "fp16_to_fp32": {},
     "qnn_preprocess": {},
     "dynamic_quant": {"int8": "QInt8", "uint8": "QUInt8"},
