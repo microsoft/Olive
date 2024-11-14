86de9198047477cdccbd1a2772ced79356e9e6d2
Linnea May <linneamay@microsoft.com>
2024-11-14 14:06:45 -0800

    clean up

--- examples/stable_diffusion/config.py
+++
@@ -1,8 +0,0 @@
-# -------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-# --------------------------------------------------------------------------
-
-vae_sample_size = 512
-unet_sample_size = 64
-cross_attention_dim = 768
