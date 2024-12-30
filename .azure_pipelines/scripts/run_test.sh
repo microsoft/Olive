9ec496d1fb0494138eafb066b6ce5e78ecc8fbdb
Xiaoyu <xiaoyuzhang@microsoft.com>
2024-12-30 23:13:47 +0000

    update script

--- .azure_pipelines/scripts/run_test.sh
+++ .azure_pipelines/scripts/run_test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Script to install dependencies and run tests
 # $1: PyTorch package name or version (e.g., torch or torch==1.12.0)
 # $2: ONNX Runtime package name or version (e.g., onnxruntime or onnxruntime==1.20.1)
