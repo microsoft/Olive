daee989c425dbf2f7a76bd8e5ad0b934e9af888a
fangyangci <fangyangci@microsoft.com>
2025-02-06 13:46:47 +0800

    format fix

--- examples/resnet/dataset_model_download.py
+++ examples/resnet/dataset_model_download.py
@@ -1,16 +1,20 @@
 # download src image + imagenet_classes.txt + _label.txt + _data.npz
-from datasets import load_dataset
-from pathlib import Path
-import numpy as np
 import os
-import torchvision.transforms as transforms
+from pathlib import Path
 
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+import numpy as np
+import torchvision.transforms as transforms
+from datasets import load_dataset
+
+preprocess = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+
 
 def download_dataset(data_dir: Path, split: str, size: int):
     image_dir = data_dir / str(split)
@@ -18,28 +22,28 @@ def download_dataset(data_dir: Path, split: str, size: int):
 
     # Load the dataset in streaming mode to avoid downloading the full dataset
     dataset = load_dataset(
-        'imagenet-1k',
+        "imagenet-1k",
         split=split,
         streaming=True,
         trust_remote_code=True,
     )
 
-    label_file = os.path.join(image_dir, '_labels.txt')
+    label_file = os.path.join(image_dir, "_labels.txt")
 
     labels = []
     images = []
-    with open(label_file, 'w') as f:
+    with open(label_file, "w") as f:
         for i, sample in enumerate(dataset):
             if i >= size:
                 break
 
-            image = sample['image']
-            label = sample['label']
+            image = sample["image"]
+            label = sample["label"]
 
-            image_path = os.path.join(image_dir, f'{i}.jpg')
+            image_path = os.path.join(image_dir, f"{i}.jpg")
             image.save(image_path)
 
-            image = image.convert('RGB')
+            image = image.convert("RGB")
             image = preprocess(image)
 
             images.append(image)
@@ -49,19 +53,24 @@ def download_dataset(data_dir: Path, split: str, size: int):
             # f.write(f'{i}.jpg,{label}\n')
 
     print(f"Images saved to {image_dir} and labels saved to {label_file}.")
-    np.savez(os.path.join(image_dir, '_data.npz'), images=np.array(images), labels=np.array(labels))
+    np.savez(os.path.join(image_dir, "_data.npz"), images=np.array(images), labels=np.array(labels))
+
 
 # Download ImageNet labels
 import urllib
+
+
 def download_classfile(data_dir: Path):
     url = "https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt"
     classFile = os.path.join(data_dir, "imagenet_classes.txt")
     urllib.request.urlretrieve(url, classFile)
 
+
 import torch
-import os
+
+
 def download_resnet_model(model_path: Path, model_name: str):
-    model = torch.hub.load('pytorch/vision:v0.10.0', model_name, pretrained=True)
+    model = torch.hub.load("pytorch/vision:v0.10.0", model_name, pretrained=True)
     # or any of these variants
     # model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
     # model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet34', pretrained=True)
--- examples/resnet/imagenet.py
+++ examples/resnet/imagenet.py
@@ -1,60 +1,65 @@
-from pathlib import Path
 from logging import getLogger
+from pathlib import Path
+
 import numpy as np
 from torch import from_numpy
 from torch.utils.data import DataLoader, Dataset
+
 from olive.data.registry import Registry
 
 logger = getLogger(__name__)
 
+
 class ImagenetDataset(Dataset):
-  def __init__(self, data_dir: str):
-    with np.load(Path(data_dir) / '_data.npz') as data:
-      self.images = from_numpy(data['images'])
-      self.labels = from_numpy(data['labels'])
+    def __init__(self, data_dir: str):
+        with np.load(Path(data_dir) / "_data.npz") as data:
+            self.images = from_numpy(data["images"])
+            self.labels = from_numpy(data["labels"])
 
-  def __len__(self):
-    return min(len(self.images), len(self.labels))
+    def __len__(self):
+        return min(len(self.images), len(self.labels))
 
-  def __getitem__(self, idx):
-    return {"input": self.images[idx]}, self.labels[idx]
+    def __getitem__(self, idx):
+        return {"input": self.images[idx]}, self.labels[idx]
 
 
 @Registry.register_dataloader()
 def imagenet_dataloader(dataset, batch_size, data_dir, **kwargs):
-  return DataLoader(ImagenetDataset(data_dir), batch_size=batch_size, shuffle=False)
+    return DataLoader(ImagenetDataset(data_dir), batch_size=batch_size, shuffle=False)
+
 
 @Registry.register_post_process()
 def imagenet_post_fun(output):
-  return output.argmax(axis=1)
+    return output.argmax(axis=1)
 
-import sys
+
 import os
+import sys
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_dir)
 
-from dataset_model_download import download_dataset, download_classfile, download_resnet_model
+from dataset_model_download import download_classfile, download_dataset, download_resnet_model
 from datasets import Split
-from pathlib import Path
 
-model_dir = Path(f'./models').resolve()
+model_dir = Path("./models").resolve()
 model_dir.mkdir(parents=True, exist_ok=True)
 
-model_name = 'resnet50'
-model_path = model_dir / f'{model_name}.pth'
+model_name = "resnet50"
+model_path = model_dir / f"{model_name}.pth"
 
 if model_path.exists():
-  logger.debug('model already downloaded.')
+    logger.debug("model already downloaded.")
 else:
-  download_resnet_model(model_path, model_name)
+    download_resnet_model(model_path, model_name)
 
-data_dir = Path('./data/imagenet').resolve()
+data_dir = Path("./data/imagenet").resolve()
 data_dir.mkdir(parents=True, exist_ok=True)
 
-data_ready = data_dir / '.complete'
+data_ready = data_dir / ".complete"
 if data_ready.exists():
-  logger.debug('Dataset already downloaded.')
+    logger.debug("Dataset already downloaded.")
 else:
-  download_dataset(data_dir, Split.VALIDATION, size=256)
-  download_classfile(data_dir)
-  data_ready.touch()
+    download_dataset(data_dir, Split.VALIDATION, size=256)
+    download_classfile(data_dir)
+    data_ready.touch()
