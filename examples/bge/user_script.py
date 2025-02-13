f6fdc65a5be50d710416917b0ed43143ca93f685
hualxie <hualxie@microsoft.com>
2025-02-13 19:47:27 +0800

    0.8574675324675324

--- examples/bge/user_script.py
+++ examples/bge/user_script.py
@@ -6,13 +6,16 @@ from typing import List
 from transformers import AutoTokenizer, BertModel
 import numpy as np
 import json
+import torch
 from pathlib import Path
+
 
 class OliveEncoder:
     def __init__(self, model, session):
         self.model = model
         self.session = session
         self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-small-en-v1.5')
+        self.total = 0
 
     def encode(self, corpus: List, **kwargs):
         if self.model.framework == Framework.ONNX:
@@ -24,18 +27,19 @@ class OliveEncoder:
             }
             model_output = self.model.run_session(self.session, model_inputs)[0]
         elif self.model.framework == Framework.PYTORCH:
-            model: HfModelHandler = self.model
-            session: BertModel = self.session # BertLMHeadModel
             encoded_input = self.tokenizer(corpus, padding=True, truncation=True, return_tensors='pt')
             model_inputs = {
                 "input_ids": encoded_input.input_ids,
                 "attention_mask": encoded_input.attention_mask,
                 "token_type_ids": encoded_input.token_type_ids
             }
-            model_output = model.run_session(session, model_inputs)
-            model_output = model_output.last_hidden_state.detach().numpy()
+            with torch.no_grad():
+                model_output = self.model.run_session(self.session, model_inputs)
+            model_output = model_output.last_hidden_state.numpy()
         # select the last hidden state of the first token (i.e., [CLS]) as the sentence embedding.
         model_output = model_output[:, 0, :]
+        self.total += len(corpus)
+        print(self.total)
         return model_output
 
 
@@ -45,7 +49,7 @@ def eval_accuracy(model: OliveModelHandler, device, execution_providers, tasks):
     evaluation = mteb.MTEB(tasks=tasks)
     oliveEncoder = OliveEncoder(model, sess)
     results = evaluation.run(oliveEncoder, output_folder=None)
-    return results[0].scores.test[0].main_score
+    return results[0].scores["test"][0]["main_score"]
 
 
 if __name__ == "__main__":
