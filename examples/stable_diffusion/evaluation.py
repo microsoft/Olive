22260c6ecc131254a34a4b27224888310685b832
Hualiang Xie (from Dev Box) <hualxie@microsoft.com>
2025-02-18 15:13:17 +0800

    Train error 704.222534 Test error 1932.550415

--- examples/stable_diffusion/evaluation.py
+++ examples/stable_diffusion/evaluation.py
@@ -6,7 +6,14 @@
 import os
 import shutil
 from pathlib import Path
+import subprocess
+from PIL import Image
+import logging
+import sys
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 def parse_args(raw_args):
     import argparse
@@ -15,6 +22,15 @@ def parse_args(raw_args):
     parser.add_argument("--save_data", action="store_true")
     return parser.parse_args(raw_args)
 
+def mse(image1, image2):
+    image1 = Image.open(image1)
+    image2 = Image.open(image2)
+
+    import numpy as np
+
+    image1 = np.array(image1, dtype=np.float32)
+    image2 = np.array(image2, dtype=np.float32)
+    return np.mean((image1 - image2) ** 2)
 
 def main(raw_args=None):
     args = parse_args(raw_args)
@@ -32,9 +48,7 @@ def main(raw_args=None):
     optimized_path = data_path / 'optimized'
 
     if args.save_data:
-        import subprocess
-
-        os.makedirs('quantize_data/', exist_ok=True)
+        os.makedirs(unoptimized_path, exist_ok=True)
         for i, prompt in enumerate(prompts):
             command = [
                 "python", "stable_diffusion.py",
@@ -51,6 +65,31 @@ def main(raw_args=None):
             # Run the command
             subprocess.run(command)
             shutil.move('result_0.png', unoptimized_path / f'{prompt}.png')
+    else:
+        os.makedirs(optimized_path, exist_ok=True)
+        train_error = 0
+        test_error = 0
+        for i, prompt in enumerate(prompts):
+            command = [
+                "python", "stable_diffusion.py",
+                "--model_id", "stabilityai/stable-diffusion-2-1-base",
+                "--provider", "qnn",
+                "--num_inference_steps", "5",
+                "--seed", "0",
+                "--prompt", prompt
+            ]
+
+            # Run the command
+            subprocess.run(command)
+            shutil.move('result_0.png', optimized_path / f'{prompt}.png')
+            error = mse(unoptimized_path / f'{prompt}.png', optimized_path / f'{prompt}.png')
+            if i < train_num:
+                train_error += error
+            else:
+                test_error += error
+
+        logger.info("Train error %f", train_error / train_num)
+        logger.info("Test error %f", test_error / (len(prompts) - train_num))
 
 
 if __name__ == "__main__":
