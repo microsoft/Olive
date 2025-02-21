0995c8d05a6b89a84fe24d5467792c99f03e757e
hualxie <hualxie@microsoft.com>
2025-02-21 15:01:50 +0800

    nit

--- examples/bge/user_script.py
+++ examples/bge/user_script.py
@@ -1,44 +1,48 @@
-from olive.model import OliveModelHandler, HfModelHandler
-from olive.constants import Framework
-from olive.workflows import run as olive_run
-from olive.engine.footprint import Footprint, FootprintNode
-import mteb
-from typing import List
-from transformers import AutoTokenizer, BertModel
-import numpy as np
 import json
-import torch
 from pathlib import Path
-from olive.data.registry import Registry
+from typing import List
+
+import mteb
+import numpy as np
+import torch
+from transformers import AutoTokenizer
+
+from olive.constants import Framework
+from olive.engine.footprint import Footprint, FootprintNode
+from olive.model import OliveModelHandler
+from olive.workflows import run as olive_run
+
 
 class OliveEncoder:
     def __init__(self, model, session):
         self.model = model
         self.session = session
-        self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-small-en-v1.5')
+        self.tokenizer = AutoTokenizer.from_pretrained("BAAI/bge-small-en-v1.5")
         self.total = 0
         self.max_len = 0
 
     def encode(self, corpus: List, **kwargs):
         if self.model.framework == Framework.ONNX:
-            encoded_input = self.tokenizer(corpus, padding="max_length", max_length=128, truncation=True, return_tensors='np')
+            encoded_input = self.tokenizer(
+                corpus, padding="max_length", max_length=128, truncation=True, return_tensors="np"
+            )
             # batch_size is 1 for static model
             model_outputs = []
             for i in range(len(corpus)):
                 model_inputs = {
-                    "input_ids": encoded_input.input_ids[i:i+1,:].astype(np.int64),
-                    "attention_mask": encoded_input.attention_mask[i:i+1,:].astype(np.int64),
-                    "token_type_ids": encoded_input.token_type_ids[i:i+1,:].astype(np.int64)
+                    "input_ids": encoded_input.input_ids[i : i + 1, :].astype(np.int64),
+                    "attention_mask": encoded_input.attention_mask[i : i + 1, :].astype(np.int64),
+                    "token_type_ids": encoded_input.token_type_ids[i : i + 1, :].astype(np.int64),
                 }
                 model_output = self.model.run_session(self.session, model_inputs)[0]
                 model_outputs.append(model_output[0])
             model_output = np.array(model_outputs)
         elif self.model.framework == Framework.PYTORCH:
-            encoded_input = self.tokenizer(corpus, padding=True, truncation=True, return_tensors='pt')
+            encoded_input = self.tokenizer(corpus, padding=True, truncation=True, return_tensors="pt")
             model_inputs = {
                 "input_ids": encoded_input.input_ids,
                 "attention_mask": encoded_input.attention_mask,
-                "token_type_ids": encoded_input.token_type_ids
+                "token_type_ids": encoded_input.token_type_ids,
             }
             self.max_len = max(self.max_len, model_inputs["input_ids"].shape[1])
             print(self.max_len)
@@ -61,55 +65,25 @@ def eval_accuracy(model: OliveModelHandler, device, execution_providers, tasks):
     return results[0].scores["test"][0]["main_score"]
 
 
-class DataLoader:
-    def __init__(self, data):
-        self.input_ids = torch.from_numpy(data["input_ids"])
-        self.attention_mask = torch.from_numpy(data["attention_mask"])
-        self.token_type_ids = torch.from_numpy(data["token_type_ids"])
-
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, idx):
-        data = {"input_ids": self.input_ids[idx],"attention_mask": self.attention_mask[idx],"token_type_ids": self.token_type_ids[idx]}
-        return data
-
-
-@Registry.register_pre_process()
-def dataset_pre_process(output_data, **kwargs):
-    cache_key = kwargs.get("cache_key")
-    max_length = kwargs.get("max_length")
-    cache_file = None
-    if cache_key:
-        cache_file = Path(f"{cache_key}.npz")
-        if cache_file.exists():
-            with np.load(Path(cache_file)) as data:
-                return DataLoader(data)
-    
-    tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-small-en-v1.5')
-    input_ids = []
-    attention_mask = []
-    token_type_ids = []
-    for item in output_data:
-        encoded_input = tokenizer(item['text'], padding="max_length", max_length=128, truncation=True, return_tensors='np')
-        input_ids.append(encoded_input.input_ids[0].astype(np.int64))
-        attention_mask.append(encoded_input.attention_mask[0].astype(np.int64))
-        token_type_ids.append(encoded_input.token_type_ids[0].astype(np.int64))
-        if max_length and len(input_ids) >= max_length:
-            break
-    
-    data = {"input_ids": np.array(input_ids), "attention_mask": np.array(attention_mask), "token_type_ids": np.array(token_type_ids)}
-    result_data = DataLoader(data)
-
-    if cache_file:
-        cache_file.parent.resolve().mkdir(parents=True, exist_ok=True)
-        np.savez(cache_file, **data)
-
-    return result_data
-
-
 if __name__ == "__main__":
-    all_ops = ["Mul", "Transpose", "Unsqueeze", "Add", "Softmax", "Gelu", "LayerNormalization", "Gather", "MatMul", "Sub", "Where", "Expand", "Gemm", "Tanh", "Reshape"]
+    # Greedy search for the best combination of ops to quantize
+    all_ops = [
+        "Mul",
+        "Transpose",
+        "Unsqueeze",
+        "Add",
+        "Softmax",
+        "Gelu",
+        "LayerNormalization",
+        "Gather",
+        "MatMul",
+        "Sub",
+        "Where",
+        "Expand",
+        "Gemm",
+        "Tanh",
+        "Reshape",
+    ]
     target_accuracy = 0.8
     with Path("bge-small-en-v1.5.json").open() as fin:
         olive_config = json.load(fin)
@@ -124,4 +98,4 @@ if __name__ == "__main__":
         print(f"Ops: {olive_config["passes"]["OnnxQuantization"]["op_types_to_quantize"]} Accuracy: {accuracy}")
         if accuracy < target_accuracy:
             olive_config["passes"]["OnnxQuantization"]["op_types_to_quantize"].remove(op)
-    print(olive_config["passes"]["OnnxQuantization"]["op_types_to_quantize"])
+    print(f"Used Ops: {olive_config["passes"]["OnnxQuantization"]["op_types_to_quantize"]}")
