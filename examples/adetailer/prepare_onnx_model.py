5ad74a49a113692caaf3b26e1cf98bd85cabe4fa
fangyangci <fangyangci@microsoft.com>
2025-02-28 21:38:25 +0800

    add title for py

--- examples/adetailer/prepare_onnx_model.py
+++ examples/adetailer/prepare_onnx_model.py
@@ -1,3 +1,7 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
 from pathlib import Path
 
 import torch
