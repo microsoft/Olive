fd1fc97a61797d3d80c1b8c958444b02237728f5
Jambay Kinley <jambaykinley@microsoft.com>
2025-03-05 00:28:51 +0000

    add unit test

--- test/unit_test/utils.py
+++ test/unit_test/utils.py
@@ -341,3 +341,27 @@ def create_raw_data(raw_data_dir, input_names, input_shapes, input_types=None, n
             data[input_name].append(data_i)
 
     return data
+
+
+def make_local_tiny_llama(save_path, model_type="hf"):
+    input_model = HfModelHandler(model_path="hf-internal-testing/tiny-random-LlamaForCausalLM")
+    loaded_model = input_model.load_model()
+    # this checkpoint has an invalid generation config that cannot be saved
+    loaded_model.generation_config.pad_token_id = 1
+
+    save_path = Path(save_path)
+    save_path.mkdir(parents=True, exist_ok=True)
+    if model_type == "hf":
+        loaded_model.save_pretrained(save_path)
+    else:
+        onnx_file_path = save_path / "model.onnx"
+        onnx_file_path.write_text("dummy onnx file")
+        loaded_model.config.save_pretrained(save_path)
+        loaded_model.generation_config.save_pretrained(save_path)
+    input_model.get_hf_tokenizer().save_pretrained(save_path)
+
+    return (
+        HfModelHandler(model_path=save_path)
+        if model_type == "hf"
+        else ONNXModelHandler(model_path=save_path, onnx_file_name="model.onnx")
+    )
