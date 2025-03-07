1d8b01a097633c033736f3456fbab3ab0e6bca78
Jambay Kinley <jambaykinley@microsoft.com>
2025-03-07 01:03:27 +0000

    StaticLLM added
    better composite model saving
    
    add unit test
    
    add doc

--- olive/passes/onnx/common.py
+++ olive/passes/onnx/common.py
@@ -5,10 +5,12 @@
 import logging
 import re
 from pathlib import Path
-from typing import Any, Dict, Optional, Type, Union
+from typing import Any, Dict, List, Optional, Type, Union
 
 import onnx
+from onnx import external_data_helper
 
+from olive.common.utils import hardlink_copy_file
 from olive.model import ONNXModelHandler
 from olive.passes.onnx.onnx_dag import OnnxDAG
 from olive.passes.pass_config import BasePassConfig, PassConfigParam
@@ -88,7 +90,7 @@ def model_proto_to_file(
     """
     output_path = Path(output_path)
     if output_path.exists():
-        logger.info("Deleting existing onnx file: %s", output_path)
+        logger.debug("Deleting existing onnx file: %s", output_path)
         output_path.unlink()
 
     # parent directory of .onnx file
@@ -189,6 +191,95 @@ def model_proto_to_olive_model(
     return olive_model
 
 
+def get_external_data_file_names(model_path: Union[str, Path]) -> List[str]:
+    """Get the external data file names from the model.
+
+    :param model_path: Path to the model file.
+    :return: List of external data file names.
+    """
+    file_names = set()
+    for tensor in external_data_helper._get_all_tensors(  # pylint: disable=W0212
+        onnx.load(model_path, load_external_data=False)
+    ):
+        if external_data_helper.uses_external_data(tensor):
+            file_names.add(external_data_helper.ExternalDataInfo(tensor).location)
+    return list(file_names)
+
+
+def change_external_data_location(model_proto: onnx.ModelProto, new_location: str):
+    """Change the external data location in the model.
+
+    :param model_proto: The model proto to modify.
+    :param new_location: The new location for the external data.
+    """
+    for tensor in external_data_helper._get_all_tensors(model_proto):  # pylint: disable=W0212
+        if external_data_helper.uses_external_data(tensor):
+            # set dummy raw_data since set_external_data expected the field
+            tensor.raw_data = b""
+            info = external_data_helper.ExternalDataInfo(tensor)
+            external_data_helper.set_external_data(
+                tensor, new_location, offset=info.offset, length=info.length, checksum=info.checksum
+            )
+
+
+def resave_model(
+    model_path: Union[str, Path],
+    new_model_path: Union[str, Path],
+    force_external_data: bool = False,
+    saved_external_files: Optional[Dict[str, str]] = None,
+) -> bool:
+    """Resave the model along with external data files.
+
+    :param model_path: Path to the original model file.
+    :param new_model_path: Path to the new model file.
+    :param force_external_data: If True, force the model to be saved with external data.
+    :param saved_external_files: A dictionary of original file paths to new file names for external data files.
+        Reuse the same file name if the the original file path is already in the dictionary.
+        Else, the new file name will be <new_model_path>.data and this dictionary will be updated with the new
+        file name.
+    :return: True if the model has external data, False otherwise.
+    """
+    model_path = Path(model_path).resolve()
+    new_model_path = Path(new_model_path).resolve()
+    assert new_model_path.suffix == ".onnx", "new_model_path must be .onnx file"
+    new_model_path.parent.mkdir(parents=True, exist_ok=True)
+
+    external_file_names = get_external_data_file_names(model_path)
+
+    if not external_file_names:
+        if force_external_data:
+            # save the model with single external data file
+            model_proto_to_file(onnx.load(model_path), new_model_path, {"save_as_external_data": True})
+            return True
+
+        # no external data, so we can just copy the model
+        hardlink_copy_file(model_path, new_model_path)
+        return False
+
+    if len(external_file_names) > 1:
+        # save the model with single external data file
+        model_proto_to_file(onnx.load(model_path), new_model_path, {"save_as_external_data": True})
+        return True
+
+    external_file_path = str(model_path.parent / external_file_names[0])
+    if saved_external_files and external_file_path in saved_external_files:
+        # already saved, model will refer to the same file
+        new_external_file_name = saved_external_files[external_file_path]
+    else:
+        new_external_file_name = f"{new_model_path.name}.data"
+        # copy the external data file to the new location
+        hardlink_copy_file(external_file_path, new_model_path.parent / new_external_file_name)
+        if saved_external_files is not None:
+            # update the saved external files mapping
+            saved_external_files[external_file_path] = new_external_file_name
+
+    # change the external data location and save the model file
+    model_proto = onnx.load(model_path, load_external_data=False)
+    change_external_data_location(model_proto, new_external_file_name)
+    model_proto_to_file(model_proto, new_model_path)
+    return True
+
+
 LORA_NAME_PATTERNS = [
     f".*[./]{name}[./]{matmul}$"
     for name in ["default_0", "default_0_1", "default", "default_1", "lora_A", "lora_B"]
@@ -209,3 +300,54 @@ def model_has_adapters(model_path: Union[str, Path]) -> bool:
         if op_type in {"MatMul", "MatMulNBits"} and any(re.match(pattern, node_name) for pattern in LORA_NAME_PATTERNS):
             return True
     return False
+
+
+def _fix_output_shapes(model_proto: onnx.ModelProto):
+    """Run shape inference on the model and update the output shapes to make them fixed."""
+    from onnxruntime.tools.onnx_model_utils import is_fixed_size_tensor
+    from onnxruntime.tools.symbolic_shape_infer import SymbolicShapeInference
+
+    # use the onnxruntime shape inference tool since it can handle large models as well as contrib ops
+    inferred_proto = SymbolicShapeInference.infer_shapes(model_proto, auto_merge=True, guess_output_rank=True)
+
+    for idx, o in enumerate(model_proto.graph.output):
+        if not is_fixed_size_tensor(o):
+            new_o = inferred_proto.graph.output[idx]
+            if is_fixed_size_tensor(new_o):
+                o.type.tensor_type.shape.CopyFrom(new_o.type.tensor_type.shape)
+
+
+def fix_dim_params(model_proto: onnx.ModelProto, dim_params: List[str], dim_values: List[int]):
+    """Fix the dimension parameters in the model.
+
+    :param dim_params: The dimension parameters to fix.
+    :param dim_values: The values to set for the dimension parameters.
+    """
+    from onnxruntime.tools.onnx_model_utils import make_dim_param_fixed
+
+    assert len(dim_params) == len(dim_values), "dim_params and dim_values must have the same number of elements."
+    assert all(i >= 0 for i in dim_values), "dim_values must be all >= 0"
+
+    for param, value in zip(dim_params, dim_values):
+        make_dim_param_fixed(model_proto.graph, param, value)
+
+    # update the output shapes to make them fixed
+    _fix_output_shapes(model_proto)
+
+
+def fix_input_shapes(model_proto: onnx.ModelProto, input_names: List[str], input_shapes: List[List[int]]):
+    """Fix the input shapes in the model.
+
+    :param input_names: The input names to fix.
+    :param input_shapes: The shapes to set for the inputs.
+    """
+    from onnxruntime.tools.onnx_model_utils import make_input_shape_fixed
+
+    assert len(input_names) == len(input_shapes), "input_names and input_shapes must have the same number of elements."
+    assert all(all(i > 0 for i in shape) for shape in input_shapes), "input_shapes must be all > 0"
+
+    for name, shape in zip(input_names, input_shapes):
+        make_input_shape_fixed(model_proto.graph, name, shape)
+
+    # update the output shapes to make them fixed
+    _fix_output_shapes(model_proto)
