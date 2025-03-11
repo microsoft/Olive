fca848de979e482f924d9a8b5a0d31cb82070159
Xiaoyu <xiaoyuzhang@microsoft.com>
2025-03-11 15:10:20 +0000

    Add hf login

--- examples/test/local/test_mobilenet.py
+++ examples/test/local/test_mobilenet.py
@@ -6,6 +6,8 @@ import os
 
 import pytest
 
+from olive.common.hf.login import huggingface_login
+
 from ..utils import check_output, get_example_dir, patch_config
 
 
@@ -17,6 +19,9 @@ def setup():
 
 def test_mobilenet():
     from olive.workflows import run as olive_run
+
+    hf_token = os.environ.get("HF_TOKEN")
+    huggingface_login(hf_token)
 
     olive_config = patch_config("config.json")
     footprint = olive_run(olive_config, tempdir=os.environ.get("OLIVE_TEMPDIR", None))
