6ab9d8bb9284a89ccc87c63ca323c0f3386f6426
Jambay Kinley <jambaykinley@microsoft.com>
2025-03-17 19:07:35 +0000

    rel-0.8.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.abspath("exts"))
 
 project = "Olive"
 copyright = "2023-2025, Olive Dev team"
-version = "latest"
+version = "0.8.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
