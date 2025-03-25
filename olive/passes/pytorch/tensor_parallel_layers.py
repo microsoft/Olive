f155794afdbf5df1735e24ba6f02d2bfe10d7743
Justin Chu <justinchuby@users.noreply.github.com>
2025-03-25 09:40:44 -0700

    [export] Use torch.onnx.ops to implement symbolic ops

--- olive/passes/pytorch/tensor_parallel_layers.py
+++ olive/passes/pytorch/tensor_parallel_layers.py
@@ -18,9 +18,15 @@ from torch import nn
 
 class AllReduce(torch.autograd.Function):  # pylint: disable=abstract-method
     @staticmethod
-    def forward(ctx, x: torch.Value) -> torch.Value:  # pylint: disable=arguments-differ
+    def forward(ctx, x: torch.Tensor) -> torch.Tensor:  # pylint: disable=arguments-differ
         if torch.onnx.is_in_onnx_export():
-            return x
+            if hasattr(torch.onnx, "ops"):
+                # torch.onnx.ops was introduced in 2.8
+                return torch.onnx.ops.symbolic(
+                    "com.microsoft::AllReduce", (x,), dtype=x.dtype, shape=x.shape, version=1
+                )
+            else:
+                return x
         dist.all_reduce(x, op=dist.ReduceOp.SUM)
         return x
 
