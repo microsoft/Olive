001ebb6ff218234928d7d5bad0a841c91c56b792
Kaunain <kaunain.ahmed@intel.com>
2025-04-25 10:43:10 -0700

    fix PR comments

--- examples/clip/openvino/user_script.py
+++ examples/clip/openvino/user_script.py
@@ -25,14 +25,6 @@ torch.cuda.manual_seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-# load pre-trained model
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-
-# load preprocessor for model input
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-
-max_length = model.config.text_config.max_position_embeddings
-
 
 def check_text_data(data):
     """Check if the given data is text-based."""
@@ -50,33 +42,37 @@ def get_pil_from_url(url):
     return image.convert("RGB")
 
 
-def collate_fn(example, image_column="image_url", text_column="caption"):
-    """Preprocess an example by loading and transforming image and text data.
+def wrap_collate_fn(processor, max_length):
+    def collate_fn(example, image_column="image_url", text_column="caption"):
+        """Preprocess an example by loading and transforming image and text data.
 
-    Check if the text data in the example is valid by calling the `check_text_data` function.
-    Downloads the image specified by the URL in the image_column by calling the `get_pil_from_url` function.
-    If there is any error during the download process, returns None.
-    Returns the preprocessed inputs with transformed image and text data.
-    """
-    assert len(example) == 1
-    example = example[0]
+        Check if the text data in the example is valid by calling the `check_text_data` function.
+        Download the image specified by the URL in the image_column by calling the `get_pil_from_url` function.
+        If there is any error during the download process, return None.
+        Return the preprocessed inputs with transformed image and text data.
+        """
+        if len(example) != 1:
+            raise ValueError(f"Expected 'example' to have exactly one element, but got {len(example)}.")
+        example = example[0]
 
-    if not check_text_data(example[text_column]):
-        raise ValueError("Text data is not valid")
+        if not check_text_data(example[text_column]):
+            raise ValueError("Text data is not valid")
 
-    url = example[image_column]
-    try:
-        image = get_pil_from_url(url)
-        h, w = image.size
-        if h == 1 or w == 1:
+        url = example[image_column]
+        try:
+            image = get_pil_from_url(url)
+            w, h = image.size
+            if h == 1 or w == 1:
+                return None
+        except Exception:
             return None
-    except Exception:
-        return None
 
-    inputs = processor(text=example[text_column], images=[image], return_tensors="pt", padding=True)
-    if inputs["input_ids"].shape[1] > max_length:
-        return None
-    return inputs
+        inputs = processor(text=example[text_column], images=[image], return_tensors="pt", padding=True)
+        if inputs["input_ids"].shape[1] > max_length:
+            return None
+        return inputs
+
+    return collate_fn
 
 
 def prepare_calibration_data(dataloader, init_steps):
@@ -94,8 +90,8 @@ def prepare_calibration_data(dataloader, init_steps):
                 with torch.no_grad():
                     data.append(
                         {
-                            "pixel_values": batch["pixel_values"].to("cpu"),
                             "input_ids": batch["input_ids"].to("cpu"),
+                            "pixel_values": batch["pixel_values"].to("cpu"),
                             "attention_mask": batch["attention_mask"].to("cpu"),
                         }
                     )
@@ -103,10 +99,17 @@ def prepare_calibration_data(dataloader, init_steps):
 
 
 @Registry.register_dataset()
-def conceptual_captions_dataset(opt_init_steps=50, max_train_samples=1000):
+def conceptual_captions_dataset(opt_init_steps=200, max_train_samples=1000, **kwargs):
     """Prepare a vision-text dataset for quantization."""
     dataset = load_dataset("google-research-datasets/conceptual_captions", trust_remote_code=True)
-    train_dataset = dataset["train"].shuffle(seed=42)
+    model_path = kwargs.get("model_path")
+    if not model_path:
+        raise ValueError("The 'model_path' parameter is required in data_configs.load_dataset_config but was not provided.")
+    model = CLIPModel.from_pretrained(model_path)
+    processor = CLIPProcessor.from_pretrained(model_path)
+    max_length = model.config.text_config.max_position_embeddings
+    train_dataset = dataset["train"].shuffle(seed=seed)
+    collate_fn = wrap_collate_fn(processor, max_length)
     dataloader = torch.utils.data.DataLoader(train_dataset, collate_fn=collate_fn, batch_size=1)
     return prepare_calibration_data(dataloader, opt_init_steps)
 
