50a271884bfac9b68d0c9f6ad58929b5a3e51213
Jambay Kinley <jambaykinley@microsoft.com>
2025-05-02 18:31:04 +0000

    overrideable input, output and logging options

--- olive/cli/run.py
+++ olive/cli/run.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------
 from argparse import ArgumentParser
 
-from olive.cli.base import BaseOliveCLICommand
+from olive.cli.base import BaseOliveCLICommand, add_input_model_options, add_logging_options, get_input_model_config
 
 
 class WorkflowRunCommand(BaseOliveCLICommand):
@@ -27,11 +27,37 @@ class WorkflowRunCommand(BaseOliveCLICommand):
                 "Configuration might also include user owned/proprietary/private pass implementations."
             ),
         )
+        add_logging_options(sub_parser, default=None)
+        add_input_model_options(
+            sub_parser.add_argument_group("Model options (not required)"),
+            enable_hf=True,
+            enable_hf_adapter=True,
+            enable_pt=True,
+            enable_onnx=True,
+            required=False,
+        )
         sub_parser.set_defaults(func=WorkflowRunCommand)
 
     def run(self):
+        from olive.common.config_utils import load_config_file
         from olive.workflows import run as olive_run
 
-        var_args = vars(self.args)
-        del var_args["func"]
-        olive_run(**var_args)
+        run_config = load_config_file(self.args.run_config)
+        if input_model_config := get_input_model_config(self.args, required=False):
+            print("Replacing input model config in run config")
+            run_config["input_model"] = input_model_config
+        for arg_key, rc_key in [("output_path", "output_dir"), ("log_level", "log_severity_level")]:
+            if (arg_value := getattr(self.args, arg_key)) is not None:
+                print(f"Replacing {rc_key} in run config with {arg_value}")
+                # remove value from engine config if it exists
+                run_config.get("engine", {}).pop(rc_key, None)
+                # add value to run config directly
+                run_config[rc_key] = arg_value
+
+        olive_run(
+            run_config,
+            setup=self.args.setup,
+            packages=self.args.packages,
+            tempdir=self.args.tempdir,
+            package_config=self.args.package_config,
+        )
