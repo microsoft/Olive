9f99e20ec6007fd12da6047d1a2b6dd020b84878
Jambay Kinley <jambaykinley@microsoft.com>
2025-07-09 16:44:03 -0700

    UT: Replace ptb_text_only with wikitext dataset (#1958)
    ## Describe your changes
    `ptb_text_only` dataset uses a script to generate the data.
    `datasets>=4.0.0` doesn't support that anymore.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    - [ ] Is this PR including examples changes? If yes, please remember to
    update [example
    documentation](https://github.com/microsoft/Olive/blob/main/docs/source/examples.md)
    in a follow-up PR.
    
    ## (Optional) Issue link

--- test/unit_test/passes/pytorch/test_sparsegpt.py
+++ test/unit_test/passes/pytorch/test_sparsegpt.py
@@ -2,10 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
-from olive.data.template import huggingface_data_config_template
 from olive.model import HfModelHandler
 from olive.passes.olive_pass import create_pass_from_dict
 from olive.passes.pytorch.sparsegpt import SparseGPT
+from test.unit_test.utils import get_wikitext_data_config
 
 
 def test_sparsegpt(tmp_path):
@@ -13,31 +13,7 @@ def test_sparsegpt(tmp_path):
     model_name = "sshleifer/tiny-gpt2"
     task = "text-generation"
     input_model = HfModelHandler(model_path=model_name, task=task)
-    component_configs = {
-        "load_dataset_config": {
-            "params": {
-                "data_name": "ptb_text_only",
-                "subset": "penn_treebank",
-                "split": "train",
-                "trust_remote_code": True,
-            }
-        },
-        "pre_process_data_config": {
-            "params": {
-                "text_cols": ["sentence"],
-                "strategy": "join-random",
-                "max_seq_len": 1024,
-                "max_samples": 1,
-                "random_seed": 42,
-                "trust_remote_code": True,
-            }
-        },
-    }
-    data_config = huggingface_data_config_template(model_name=model_name, task=task, **component_configs)
-    config = {
-        "sparsity": [2, 4],
-        "data_config": data_config,
-    }
+    config = {"sparsity": [2, 4], "data_config": get_wikitext_data_config(model_name)}
 
     p = create_pass_from_dict(SparseGPT, config, disable_search=True)
     output_folder = str(tmp_path / "sparse")
