4d18aac2549a9f11808c860ceab0374bb9b97bca
Xiaoyu <xiaoyuzhang@microsoft.com>
2025-07-18 16:09:36 -0700

    Update default target_opset to 21 in OnnxConversion

--- olive/passes/onnx/conversion.py
+++ olive/passes/onnx/conversion.py
@@ -58,7 +58,7 @@ class OnnxConversion(Pass):
             **get_user_script_data_config(),
             **get_external_data_config(),
             "target_opset": PassConfigParam(
-                type_=int, default_value=20, description="The version of the default (ai.onnx) opset to target."
+                type_=int, default_value=21, description="The version of the default (ai.onnx) opset to target."
             ),
             "use_dynamo_exporter": PassConfigParam(
                 type_=bool, default_value=False, description="Whether to use dynamo_export API to export ONNX model."
