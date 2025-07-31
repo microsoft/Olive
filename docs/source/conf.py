b2d32b22fe0a9d7d5f47b51db8298210f10615a0
Xiaoyu <xiaoyuzhang@microsoft.com>
2025-07-31 00:52:46 +0000

    Release branch 0.9.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.abspath("exts"))
 
 project = "Olive"
 copyright = "2023-2025, Olive Dev team"
-version = "latest"
+version = "0.9.2"
 release = version
 
 # -- General configuration ---------------------------------------------------
