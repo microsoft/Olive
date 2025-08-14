be89d7c186f23325cb7e46863374b2a1d0f1ea97
Jambay Kinley <jambaykinley@microsoft.com>
2025-08-14 18:46:28 +0000

    update test

--- test/workflows/test_workflow_run.py
+++ test/workflows/test_workflow_run.py
@@ -4,7 +4,6 @@ from unittest.mock import patch
 
 import pytest
 
-from olive.data.registry import Registry
 from olive.workflows import run as olive_run
 from test.utils import (
     get_pytorch_model,
@@ -12,12 +11,6 @@ from test.utils import (
     get_pytorch_model_io_config,
     pytorch_model_loader,
 )
-
-
-@Registry.register_dataloader()
-def workflow_run_test_dataloader(dataset, batch_size):
-    return None
-
 
 INPUT_MODEL_CONFIG = {
     "type": "PyTorchModel",
@@ -41,36 +34,8 @@ EVALUATORS_CONFIG = {
     ]
 }
 
-DATA_CONFIGS = [
-    {
-        "name": "qat_train_data_config",
-        "type": "DummyDataContainer",
-        "load_dataset_config": {"type": "simple_dataset", "params": {"data_dir": "data"}},
-        "pre_process_data_config": {"type": "skip_pre_process"},
-        "post_process_data_config": {"type": "skip_post_process"},
-        "dataloader_config": {"type": "workflow_run_test_dataloader", "params": {"batch_size": 100}},
-    },
-    {
-        "name": "qat_val_data_config",
-        "type": "DummyDataContainer",
-        "load_dataset_config": {"type": "simple_dataset", "params": {"data_dir": "data"}},
-        "pre_process_data_config": {"type": "skip_pre_process"},
-        "post_process_data_config": {"type": "skip_post_process"},
-        "dataloader_config": {"type": "workflow_run_test_dataloader", "params": {"batch_size": 100}},
-    },
-]
-
 PASS_CONFIG = {
-    "qat": {
-        "type": "QuantizationAwareTraining",
-        "config": {
-            "train_data_config": "qat_train_data_config",
-            "val_data_config": "qat_val_data_config",
-            "num_epochs": 1,
-            "modules_to_fuse": [["conv1", "bn1"], ["conv2", "bn2"], ["conv3", "bn3"]],
-            "qconfig_func": "create_qat_config",
-        },
-    },
+    "gptq": {"type": "Gptq"},
 }
 
 
@@ -79,29 +44,22 @@ PASS_CONFIG = {
     [
         {
             "input_model": INPUT_MODEL_CONFIG,
-            "data_configs": DATA_CONFIGS,
             "evaluators": {"common_evaluator": EVALUATORS_CONFIG},
             "passes": PASS_CONFIG,
             "engine": {"evaluator": "common_evaluator"},
         },
         {
             "input_model": INPUT_MODEL_CONFIG,
-            "data_configs": DATA_CONFIGS,
             "passes": PASS_CONFIG,
             "engine": {"evaluator": EVALUATORS_CONFIG},
         },
     ],
 )
-@patch("olive.passes.pytorch.quantization_aware_training.QuantizationAwareTraining._run_for_config")
+@patch("olive.passes.pytorch.gptq.Gptq._run_for_config")
 @patch("olive.systems.local.ModelConfig.from_json")
 @patch("olive.engine.engine.ModelConfig.to_json")
 def test_run_without_ep(mock_model_to_json, mock_model_from_json, mock_run, config_test, tmp_path):
-    user_script = tmp_path / "user_script.py"
-    with user_script.open("w"):
-        pass
-
     config = deepcopy(config_test)
-    config["passes"]["qat"]["config"]["user_script"] = str(user_script)
     config["engine"]["cache_dir"] = str(tmp_path / "cache")
     config["engine"]["output_dir"] = str(tmp_path / "output")
 
@@ -119,7 +77,6 @@ def test_run_packages():
         "input_model": INPUT_MODEL_CONFIG,
         "evaluators": {"common_evaluator": EVALUATORS_CONFIG},
         "passes": PASS_CONFIG,
-        "data_configs": DATA_CONFIGS,
         "engine": {"evaluator": "common_evaluator"},
     }
 
@@ -131,7 +88,7 @@ def test_run_packages():
     assert (requirements_file_path).exists()
     with (requirements_file_path).open() as f:
         file = f.read()
-        assert file == "pytorch-lightning\nonnxruntime"
+        assert file == "onnxruntime"
 
     # cleanup
     requirements_file_path.unlink()
