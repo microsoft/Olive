e6990b4768a98072476b45537637433329fd8789
Justin Chu <justinchuby@users.noreply.github.com>
2025-08-18 10:24:43 -0700

    Update test/passes/onnx/test_conversion.py

--- test/passes/onnx/test_conversion.py
+++ test/passes/onnx/test_conversion.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
+import platform
 import shutil
 from itertools import chain
 from pathlib import Path
