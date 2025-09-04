ecb2cecedcb80071ad08315a1e21aa09b2f2b223
Bhagirath Mehta <bhamehta@microsoft.com>
2025-09-04 18:26:47 +0000

    Fmt

--- olive/cli/convert_adapters.py
+++ olive/cli/convert_adapters.py
@@ -8,10 +8,10 @@ from typing import TYPE_CHECKING
 
 from olive.cli.base import BaseOliveCLICommand, add_logging_options, add_telemetry_options
 from olive.common.utils import WeightsFileFormat, save_weights
+from olive.telemetry.telemetry_events import action
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
-from olive.telemetry.telemetry_events import action
 
 
 class ConvertAdaptersCommand(BaseOliveCLICommand):
