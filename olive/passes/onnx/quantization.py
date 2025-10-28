f1544e86d73fe3ed4b93a99dd8611a4e70233e49
Jambay Kinley <jambaykinley@microsoft.com>
2025-10-28 15:53:55 -0700

    Static Quantization: Patch `MinMaxCalibrator`, Handle `extra_options` for QNN EP (#2229)
    ## Describe your changes
    - The MixMax calibrator stores all the outputs of each session run as
    intermediate outputs. However, it only needs the augmented min and max
    values. This causes the memory usage to unnecessarily grow with each
    iteration of the calibration data, especially for llms which have large
    outputs (logits and kv cache). This has been addressed in ort with
    https://github.com/microsoft/onnxruntime/pull/26321
    - Patching in Olive also with a more general solution by removing the
    original outputs from the augmented graph as well as unused leaf nodes
    to reduce memory and compute usage.
    - The static quantization pass did not handle `extra_options` when the
    EP is QNN. Fixing this as well.
    
    ## Checklist before requesting a review
    - [ ] Add unit tests for this change.
    - [ ] Make sure all tests can pass.
    - [ ] Update documents if necessary.
    - [ ] Lint and apply fixes to your code by running `lintrunner -a`
    - [ ] Is this a user-facing change? If yes, give a description of this
    change to be included in the release notes.
    
    ## (Optional) Issue link

--- olive/passes/onnx/quantization.py
+++ olive/passes/onnx/quantization.py
@@ -252,6 +252,42 @@ _param_extra_options_mapping = {
 }
 
 
+def maybe_patch_min_max_calibrater():
+    from onnxruntime import __version__ as OrtVersion
+
+    if version.parse(OrtVersion).release >= version.parse("1.24.0").release:
+        # no need to patch for onnxruntime<1.24.0
+        return
+
+    from onnxruntime.quantization.calibrate import MinMaxCalibrater
+
+    original_augment_graph = MinMaxCalibrater.augment_graph
+    if original_augment_graph.__name__ == "patched_augment_graph":
+        return
+
+    from olive.passes.onnx.onnx_dag import OnnxDAG
+
+    def patched_augment_graph(self):
+        original_augment_graph(self)
+
+        dag = OnnxDAG(onnx.load(self.augmented_model_path, load_external_data=False))
+        for o_name in list(self.model_original_outputs):
+            # remove the output since we don't need it for calibration
+            dag.remove_output(o_name)
+            self.model_original_outputs.remove(o_name)
+            self.num_model_outputs -= 1
+
+            # if the producer has no consumers, remove it
+            producer = dag.get_producer(o_name)
+            if len(dag.get_consumers(producer)) == 0:
+                # remove the producer if it has no consumers
+                dag.remove_node(producer)
+        dag.update()
+        onnx.save(dag.model, self.augmented_model_path)
+
+    MinMaxCalibrater.augment_graph = patched_augment_graph
+
+
 class OnnxQuantization(Pass):
     """Quantize ONNX model with static/dynamic quantization techniques."""
 
@@ -458,6 +494,7 @@ class OnnxQuantization(Pass):
 
         if is_static:
             run_config = self.get_static_run_config(model, config, run_config, ort_less_than_1_21)
+            maybe_patch_min_max_calibrater()
             try:
                 quantize_static(
                     model_input=model.model_path,
@@ -563,7 +600,7 @@ class OnnxQuantization(Pass):
                 get_qdq_config_kwargs[qdq_key] = extra_options[key]
                 # remove the key from extra_options
                 extra_options.pop(key, None)
-        if extra_options:
+        if extra_options and not is_qnn_ep:
             get_qdq_config_kwargs["extra_options"] = extra_options
         # tensor_quant_overrides is init_overrides in qnn
         if is_qnn_ep:
@@ -577,6 +614,11 @@ class OnnxQuantization(Pass):
 
         # get the qdq config
         qdq_config = get_qdq_config(**get_qdq_config_kwargs)
+
+        # allow user's extra_options to override the qdq_config extra_options
+        # same as the non-qnn path
+        if is_qnn_ep and extra_options:
+            qdq_config.extra_options.update(extra_options)
 
         # override the run_config with qdq_config
         new_run_config = {k: v for k, v in inspect.getmembers(qdq_config) if not k.startswith("_")}
@@ -594,6 +636,7 @@ class OnnxQuantization(Pass):
                     set(new_run_config["op_types_to_quantize"]) - set(config.op_types_to_exclude)
                 )
 
+        logger.debug("QDQ quantization run config: %s", new_run_config)
         return new_run_config
 
 
