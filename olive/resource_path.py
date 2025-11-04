d5fcec3d4e6e26fc4cdf0538c37e03a7bd866577
Xiaoyu <xiaoyuzhang@microsoft.com>
2025-11-04 18:15:03 +0000

    Fix save to dir

--- olive/resource_path.py
+++ olive/resource_path.py
@@ -238,14 +238,34 @@ class LocalResourcePath(ResourcePath):
         else:
             new_path_name = self.config.path.name
         new_path = dir_path / new_path_name
-        _overwrite_helper(new_path, overwrite)
+
         # is the resource a file or a folder
         is_file = Path(self.config.path).is_file()
+
+        # Handle overwrite logic based on actual destination
+        if is_file or not flatten:
+            # Actual destination is new_path
+            _overwrite_helper(new_path, overwrite)
+        else:
+            # flatten=True and is folder: destination is dir_path
+            # Check for conflicts with individual files in source
+            if overwrite:
+                for item in Path(self.config.path).iterdir():
+                    target = dir_path / item.name
+                    if target.exists():
+                        if target.is_file():
+                            target.unlink()
+                        else:
+                            shutil.rmtree(target)
+
         # copy the resource to the new path
         if is_file:
             shutil.copy(self.config.path, new_path)
         else:
-            copy_dir(self.config.path, dir_path if flatten else new_path)
+            if flatten:
+                copy_dir(self.config.path, dir_path, dirs_exist_ok=True)
+            else:
+                copy_dir(self.config.path, new_path)
 
         return str(new_path)
 
