30dad30bc3d9d41507bc8baf81b3dbefa6acdb4c
Jambay Kinley <jambaykinley@microsoft.com>
2026-07-01 21:03:41 +0000

    suppress warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ source_suffix = {
 templates_path = ["_templates"]
 exclude_patterns = []
 
-suppress_warnings = ["myst.xref_missing"]
+suppress_warnings = ["myst.xref_missing", "myst.domains"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
