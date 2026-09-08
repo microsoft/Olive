e1663a2885d564ba2d8641af451242ee91f02a8d
Xiaoyu Zhang <xiaoyuzhang@microsoft.com>
2026-09-08 11:57:06 -0700

    Refactor ONNX graph surgery base classes
    Move the surgeon registry and shared proto/rewrite-rule base classes into a dedicated package so new transformations can be implemented in focused modules without growing graph_surgeries.py.
    
    Signed-off-by: Xiaoyu Zhang <xiaoyuzhang@microsoft.com>

---
+++ olive/passes/onnx/graph_surgery/__init__.py
@@ -0,0 +1,7 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+from olive.passes.onnx.graph_surgery.base import ProtoSurgeon, RewriteRuleSurgeon, Surgeon
+
+__all__ = ["ProtoSurgeon", "RewriteRuleSurgeon", "Surgeon"]
